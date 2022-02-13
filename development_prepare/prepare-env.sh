4533787cec2ffb884b6f11fda38774a6699484da
splitti <45425109+splitti@users.noreply.github.com>
2022-02-13 02:31:15 +0100

    Prep

--- development_prepare/prepare-env.sh
+++ development_prepare/prepare-env.sh
@@ -17,11 +17,12 @@ percentBar ()  {
 
 Y=15
 X=4
+LOG="/tmp/prepage.log"
 
 FORMAT="\e[48;5;23;38;5;41m%s\e[0m"
 
 # Make it nice
-sudo apt-get install figlet lolcat -y >> /tmp/prepage.log
+sudo apt-get install figlet lolcat -y >> ${LOG} 2>>&1
 clear
 figlet -f standard -c -m 1  " MuPiBox Preperation" | lolcat
 tput cup $Y $X
@@ -31,7 +32,7 @@ percentBar 0 60 bar
 printf ${FORMAT} "$bar"
 
 # Get missing packages
-sudo apt-get install libasound2 jq samba -y >> /tmp/prepage.log
+sudo apt-get install git libasound2 jq samba -y >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Install nodeJS 16 \n    "
@@ -40,9 +41,9 @@ percentBar 10 60 bar
 printf ${FORMAT} "$bar"
 
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - >> /tmp/prepage.log
-sudo apt-get install -y nodejs >> /tmp/prepage.log
-sudo npm install -g @ionic/cli >> /tmp/prepage.log
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - >> ${LOG} 2>>&1
+sudo apt-get install -y nodejs >> ${LOG} 2>>&1
+sudo npm install -g @ionic/cli >> ${LOG} 2>>&1
 
 
 tput cup $Y $X
@@ -53,16 +54,16 @@ printf ${FORMAT} "$bar"
 
 
 # Create Directorys
-mkdir -p ~/.mupibox >> /tmp/prepage.log
-mkdir -p ~/MuPiBox/media >> /tmp/prepage.log
-mkdir ~/MuPiBox/tts_files >> /tmp/prepage.log
-mkdir -p ~/MuPiBox/sysmedia/sound >> /tmp/prepage.log
-mkdir ~/MuPiBox/sysmedia/images >> /tmp/prepage.log
-mkdir ~/MuPiBox/media >> /tmp/prepage.log
-sudo mkdir /usr/local/bin/mupibox >> /tmp/prepage.log
-sudo mkdir /etc/spotifyd >> /tmp/prepage.log
-sudo mkdir /etc/mupibox >> /tmp/prepage.log
-sudo mkdir /var/log/mupibox/ >> /tmp/prepage.logv
+mkdir -p ~/.mupibox >> ${LOG} 2>>&1
+mkdir -p ~/MuPiBox/media >> ${LOG} 2>>&1
+mkdir ~/MuPiBox/tts_files >> ${LOG} 2>>&1
+mkdir -p ~/MuPiBox/sysmedia/sound >> ${LOG} 2>>&1
+mkdir ~/MuPiBox/sysmedia/images >> ${LOG} 2>>&1
+mkdir ~/MuPiBox/media >> ${LOG} 2>>&1
+sudo mkdir /usr/local/bin/mupibox >> ${LOG} 2>>&1
+sudo mkdir /etc/spotifyd >> ${LOG} 2>>&1
+sudo mkdir /etc/mupibox >> ${LOG} 2>>&1
+sudo mkdir /var/log/mupibox/ >> ${LOG} 2>>&1v
 sleep 1
 
 # Boot
@@ -75,19 +76,26 @@ percentBar 30 60 bar
 printf ${FORMAT} "$bar"
 
 # Sources
-cd ~/.mupibox >> /tmp/prepage.log
-git clone https://github.com/derhuerst/mplayer-wrapper >> /tmp/prepage.log
-git clone https://github.com/zlargon/google-tts >> /tmp/prepage.log
-wget https://github.com/Thyraz/Sonos-Kids-Controller/archive/refs/tags/V1.6.zip >> /tmp/prepage.log
-unzip V1.6.zip >> /tmp/prepage.log
-rm V1.6.zip >> /tmp/prepage.log
-mv Sonos-Kids-Controller-1.6/ Sonos-Kids-Controller-master >> /tmp/prepage.log
-#npm install
-#npm audit fix
-#ionic build --prod --verbose
-mkdir -p ~/.mupibox/Sonos-Kids-Controller-master/www/cover >> /tmp/prepage.log
-#sudo npm install pm2 -g 
-#pm2 startup
+cd ~/.mupibox >> /tmp/prepare.log
+git clone https://github.com/derhuerst/mplayer-wrapper >> ${LOG} 2>>&1
+cd mplayer-wrapper >> ${LOG} 2>>&1
+npm install >> ${LOG} 2>>&1
+cd ~ >> ${LOG} 2>>&1
+git clone https://github.com/zlargon/google-tts >> ${LOG} 2>>&1
+cd google-tts/ >> ${LOG} 2>>&1
+npm install --save >> ${LOG} 2>>&1
+npm audit fix >> ${LOG} 2>>&1
+wget https://github.com/Thyraz/Sonos-Kids-Controller/archive/refs/tags/V1.6.zip >> ${LOG} 2>>&1
+unzip V1.6.zip >> ${LOG} 2>>&1
+rm V1.6.zip >> ${LOG} 2>>&1
+mv Sonos-Kids-Controller-1.6/ Sonos-Kids-Controller-master >> ${LOG} 2>>&1
+npm install  >> ${LOG} 2>>&1
+npm audit fix  >> ${LOG} 2>>&1
+ionic build --prod --verbose  >> ${LOG} 2>>&1
+mkdir -p ~/.mupibox/Sonos-Kids-Controller-master/www/cover >> ${LOG} 2>>&1
+sudo npm install pm2 -g  >> ${LOG} 2>>&1
+sudo apt-get remove git  >> ${LOG} 2>>&1
+#pm2 startup  >> ${LOG} 2>>&1
 # ENVIRONMENT VAR
 #cd ~/.mupibox/Sonos-Kids-Controller-master/
 #pm2 start server.js
@@ -99,9 +107,9 @@ percentBar 50 60 bar
 printf ${FORMAT} "$bar"
 
 # Binaries
-sudo wget https://github.com/splitti/MuPiBox/raw/main/bin/fbv/fbv -O /usr/bin/fbv  >> /tmp/prepage.log
-sudo wget https://github.com/splitti/MuPiBox/raw/main/bin/spotifyd/0.3.3/dietpi8_64bit/spotifyd -O /usr/bin/spotifyd  >> /tmp/prepage.log
-sudo chmod 755 /usr/bin/fbv /usr/bin/spotifyd
+sudo wget https://github.com/splitti/MuPiBox/raw/main/bin/fbv/fbv -O /usr/bin/fbv  >> ${LOG} 2>>&1
+sudo wget https://github.com/splitti/MuPiBox/raw/main/bin/spotifyd/0.3.3/dietpi8_64bit/spotifyd -O /usr/bin/spotifyd  >> ${LOG} 2>>&1
+sudo chmod 755 /usr/bin/fbv /usr/bin/spotifyd >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Copy Services \n    "
@@ -109,10 +117,10 @@ percentBar 55 60 bar
 printf ${FORMAT} "$bar"
 
 # Services
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/mupi_change_checker.service -O /etc/systemd/system/mupi_change_checker.service >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/mupi_idle_shutdown.service -O /etc/systemd/system/mupi_idle_shutdown.service >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/mupi_splash.service -O /etc/systemd/system/mupi_splash.service >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/spotifyd.service -O /etc/systemd/system/spotifyd.service >> /tmp/prepage.log
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/mupi_change_checker.service -O /etc/systemd/system/mupi_change_checker.service >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/mupi_idle_shutdown.service -O /etc/systemd/system/mupi_idle_shutdown.service >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/mupi_splash.service -O /etc/systemd/system/mupi_splash.service >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/services/spotifyd.service -O /etc/systemd/system/spotifyd.service >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Copy dietpi-configs \n    "
@@ -120,9 +128,9 @@ percentBar 60 60 bar
 printf ${FORMAT} "$bar"
 
 # DietPi-Configs
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/98-dietpi-disable_dpms.conf -O /etc/X11/xorg.conf.d/98-dietpi-disable_dpms.conf >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/asound.conf -O /etc/asound.conf >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/smb.conf -O /etc/samba/smb.conf >> /tmp/prepage.log
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/98-dietpi-disable_dpms.conf -O /etc/X11/xorg.conf.d/98-dietpi-disable_dpms.conf >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/asound.conf -O /etc/asound.conf >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/smb.conf -O /etc/samba/smb.conf >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Copy spotify-configs \n    "
@@ -130,9 +138,9 @@ percentBar 65 60 bar
 printf ${FORMAT} "$bar"
 
 # Spotify-Configs
-wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/www.json -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/config.json >> /tmp/prepage.log
-wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/spotifycontroller.json -O /home/dietpi/.mupibox/spotifycontroller-main/config/config.json >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/spotifyd.conf -O /etc/spotifyd/spotifyd.conf >> /tmp/prepage.log
+wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/www.json -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/config.json >> ${LOG} 2>>&1
+wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/spotifycontroller.json -O /home/dietpi/.mupibox/spotifycontroller-main/config/config.json >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/spotifyd.conf -O /etc/spotifyd/spotifyd.conf >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Copy media-files \n    "
@@ -140,12 +148,12 @@ percentBar 70 60 bar
 printf ${FORMAT} "$bar"
 
 # Splash and Media
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/splash.txt -O /boot/splash.txt >> /tmp/prepage.log
-sudo wget https://gitlab.com/DarkElvenAngel/initramfs-splash/-/raw/master/boot/initramfs.img -O /boot/initramfs.img >> /tmp/prepage.log
-wget https://github.com/splitti/MuPiBox/raw/main/media/images/goodbye.png -O /home/dietpi/MuPiBox/sysmedia/images/goodbye.png >> /tmp/prepage.log
-sudo wget https://github.com/splitti/MuPiBox/raw/main/media/images/splash.png -O /boot/splash.png >> /tmp/prepage.log
-wget https://github.com/splitti/MuPiBox/raw/main/media/sound/shutdown.wav -O /home/dietpi/MuPiBox/sysmedia/sound/shutdown.wav >> /tmp/prepage.log
-wget https://github.com/splitti/MuPiBox/raw/main/media/sound/startup.wav -O /home/dietpi/MuPiBox/sysmedia/sound/startup.wav >> /tmp/prepage.log
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/splash.txt -O /boot/splash.txt >> ${LOG} 2>>&1
+sudo wget https://gitlab.com/DarkElvenAngel/initramfs-splash/-/raw/master/boot/initramfs.img -O /boot/initramfs.img >> ${LOG} 2>>&1
+wget https://github.com/splitti/MuPiBox/raw/main/media/images/goodbye.png -O /home/dietpi/MuPiBox/sysmedia/images/goodbye.png >> ${LOG} 2>>&1
+sudo wget https://github.com/splitti/MuPiBox/raw/main/media/images/splash.png -O /boot/splash.png >> ${LOG} 2>>&1
+wget https://github.com/splitti/MuPiBox/raw/main/media/sound/shutdown.wav -O /home/dietpi/MuPiBox/sysmedia/sound/shutdown.wav >> ${LOG} 2>>&1
+wget https://github.com/splitti/MuPiBox/raw/main/media/sound/startup.wav -O /home/dietpi/MuPiBox/sysmedia/sound/startup.wav >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Copy mupibox-files \n    "
@@ -153,14 +161,14 @@ percentBar 75 60 bar
 printf ${FORMAT} "$bar"
 
 # MuPiBox
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/mupiboxconfig.json -O /etc/mupibox/mupiboxconfig.json >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/change_checker.sh -O /usr/local/bin/mupibox/change_checker.sh >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/idle_shutdown.sh -O /usr/local/bin/mupibox/idle_shutdown.sh >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/m3u_generator.sh -O /usr/local/bin/mupibox/m3u_generator.sh >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/setting_update.sh -O /usr/local/bin/mupibox/setting_update.sh >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/software_shutdown.sh -O /usr/local/bin/mupibox/software_shutdown.sh >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/splash_screen.sh -O /usr/local/bin/mupibox/splash_screen.sh >> /tmp/prepage.log
-sudo chmod 755 /usr/local/bin/mupibox/change_checker.sh /usr/local/bin/mupibox/idle_shutdown.sh /usr/local/bin/mupibox/m3u_generator.sh /usr/local/bin/mupibox/setting_update.sh /usr/local/bin/mupibox/software_shutdown.sh /usr/local/bin/mupibox/splash_screen.sh >> /tmp/prepage.log
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/mupiboxconfig.json -O /etc/mupibox/mupiboxconfig.json >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/change_checker.sh -O /usr/local/bin/mupibox/change_checker.sh >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/idle_shutdown.sh -O /usr/local/bin/mupibox/idle_shutdown.sh >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/m3u_generator.sh -O /usr/local/bin/mupibox/m3u_generator.sh >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/setting_update.sh -O /usr/local/bin/mupibox/setting_update.sh >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/software_shutdown.sh -O /usr/local/bin/mupibox/software_shutdown.sh >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/mupibox/splash_screen.sh -O /usr/local/bin/mupibox/splash_screen.sh >> ${LOG} 2>>&1
+sudo chmod 755 /usr/local/bin/mupibox/change_checker.sh /usr/local/bin/mupibox/idle_shutdown.sh /usr/local/bin/mupibox/m3u_generator.sh /usr/local/bin/mupibox/setting_update.sh /usr/local/bin/mupibox/software_shutdown.sh /usr/local/bin/mupibox/splash_screen.sh >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Copy onoffshim-scripts \n    "
@@ -168,9 +176,9 @@ percentBar 80 60 bar
 printf ${FORMAT} "$bar"
 
 # OnOffShim
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/OnOffShim/off_trigger.sh -O /var/lib/dietpi/postboot.d/off_trigger.sh >> /tmp/prepage.log
-sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/OnOffShim/poweroff.sh -O /usr/lib/systemd/system-shutdown/poweroff.sh >> /tmp/prepage.log
-sudo chmod 775 /usr/lib/systemd/system-shutdown/poweroff.sh /var/lib/dietpi/postboot.d/off_trigger.sh >> /tmp/prepage.log
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/OnOffShim/off_trigger.sh -O /var/lib/dietpi/postboot.d/off_trigger.sh >> ${LOG} 2>>&1
+sudo wget https://raw.githubusercontent.com/splitti/MuPiBox/main/scripts/OnOffShim/poweroff.sh -O /usr/lib/systemd/system-shutdown/poweroff.sh >> ${LOG} 2>>&1
+sudo chmod 775 /usr/lib/systemd/system-shutdown/poweroff.sh /var/lib/dietpi/postboot.d/off_trigger.sh >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "Enable services \n    "
@@ -178,24 +186,24 @@ percentBar 80 60 bar
 printf ${FORMAT} "$bar"
 
 # Enable Services
-sudo systemctl daemon-reload >> /tmp/prepage.log
-sudo systemctl enable mupi_change_checker.service >> /tmp/prepage.log
-sudo systemctl start mupi_change_checker.service >> /tmp/prepage.log
-sudo systemctl enable mupi_idle_shutdown.service >> /tmp/prepage.log
-sudo systemctl start mupi_idle_shutdown.service >> /tmp/prepage.log
-sudo systemctl enable mupi_splash.service >> /tmp/prepage.log
-sudo systemctl start mupi_splash.service >> /tmp/prepage.log
-sudo systemctl enable spotifyd.service >> /tmp/prepage.log
-sudo systemctl start spotifyd.service >> /tmp/prepage.log
-sudo systemctl enable smbd.service >> /tmp/prepage.log
-sudo systemctl start smbd.service >> /tmp/prepage.log
+sudo systemctl daemon-reload >> ${LOG} 2>>&1
+#sudo systemctl enable mupi_change_checker.service >> ${LOG} 2>>&1
+#sudo systemctl start mupi_change_checker.service >> ${LOG} 2>>&1
+#sudo systemctl enable mupi_idle_shutdown.service >> ${LOG} 2>>&1
+#sudo systemctl start mupi_idle_shutdown.service >> ${LOG} 2>>&1
+#sudo systemctl enable mupi_splash.service >> ${LOG} 2>>&1
+#sudo systemctl start mupi_splash.service >> ${LOG} 2>>&1
+#sudo systemctl enable spotifyd.service >> ${LOG} 2>>&1
+#sudo systemctl start spotifyd.service >> ${LOG} 2>>&1
+sudo systemctl enable smbd.service >> ${LOG} 2>>&1
+sudo systemctl start smbd.service >> ${LOG} 2>>&1
 
 tput cup $Y $X
 printf "SYSTEM IS PREPARED \n    "
 percentBar 95 60 bar
 printf ${FORMAT} "$bar"
 # ENV
-sudo env PATH=$PATH:/usr/local/bin/mupibox >> /tmp/prepage.log
+sudo env PATH=$PATH:/usr/local/bin/mupibox >> ${LOG} 2>>&1
 sleep 1
 
 tput cup $Y $X
@@ -203,3 +211,5 @@ printf "Set environment-variable \n    "
 percentBar 100 60 bar
 printf ${FORMAT} "$bar"
 echo " "
+echo "    Logfile: ${LOG}"
+
