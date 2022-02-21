b4fa50111a87ca6d92f432d668a06d3540eee047
splitti <45425109+splitti@users.noreply.github.com>
2022-02-21 23:00:51 +0100

    Bugfix

--- scripts/mupibox/change_checker.sh
+++ scripts/mupibox/change_checker.sh
@@ -2,18 +2,19 @@
 #
 # Check for changes in MuPiBox Media directory
 
-MUPIBOX_CONFIG="/etc/mupibox/mupiboxconfig.json"
-CHECK_TIMER=`/usr/bin/jq -r .mupibox.mediaCheckTimer ${CONFIG}`
+CONFIG="/etc/mupibox/mupiboxconfig.json"
+CHECK_TIMER=$(/usr/bin/jq -r .mupibox.mediaCheckTimer ${CONFIG})
 
 while true
 do
   for dir in /home/dietpi/MuPiBox/media/* ; do
     difference=$(($(date +%s) - $(stat -c %Z "${dir}") - ${CHECK_TIMER}))
-	if ((${difference} < 1))
-	then
-	  /usr/local/bin/mupibox/./m3u_generator.sh &
-	  break
-	fi
+        if ((${difference} < 1))
+        then
+          /usr/local/bin/mupibox/./m3u_generator.sh &
+          break
+        fi
   done
   sleep ${CHECK_TIMER}
 done
+
