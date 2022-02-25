db4b46701443fc50613e58b24a1ef2557c2559f5
splitti <45425109+splitti@users.noreply.github.com>
2022-02-26 00:04:42 +0100

    Update

--- autosetup/autostart.sh
+++ autosetup/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "sleep 10" >> /home/dietpi/.bashrc
-echo "sudo sh -c 'su - dietpi -s 'cd; curl https://raw.githubusercontent.com/splitti/MuPiBox/main/autosetup/autosetup.sh | bash'" >> /home/dietpi/.bashrc
+echo "cd; curl https://raw.githubusercontent.com/splitti/MuPiBox/main/autosetup/autosetup.sh | bash" >> /home/dietpi/.bashrc
 reboot
--- scripts/mupibox/mupi_startup.sh
+++ scripts/mupibox/mupi_startup.sh
@@ -14,5 +14,5 @@ sudo /bin/echo out > /sys/class/gpio/gpio${LED_PIN}/direction
 sudo /bin/echo 1 > /sys/class/gpio/gpio${LED_PIN}/value
 
 # Set default Volume and play start sound
-/usr/bin/amixer sset ${AUDIO_DEVICE} ${START_VOLUME}%
-/usr/bin/mplayer ${START_SOUND}
+su - dietpi -c '/usr/bin/amixer sset ${AUDIO_DEVICE} ${START_VOLUME}%'
+su - dietpi -c '/usr/bin/mplayer ${START_SOUND}'
