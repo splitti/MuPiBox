4e78848c6bb31c024efa503d19b04f3322b68083
splitti <45425109+splitti@users.noreply.github.com>
2022-02-27 02:46:40 +0100

    Update autostart.sh

--- autosetup/autostart.sh
+++ autosetup/autostart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "echo '\n\n Please wait, MuPiBox-Installer starts soon...'0 && sleep 10" >> /home/dietpi/.bashrc
+echo "echo '' && echo '' && echo 'Please wait, MuPiBox-Installer starts soon...' && sleep 10" >> /home/dietpi/.bashrc
 echo "cd; curl https://raw.githubusercontent.com/splitti/MuPiBox/main/autosetup/autosetup.sh | nice -n 19 bash" >> /home/dietpi/.bashrc
 reboot
