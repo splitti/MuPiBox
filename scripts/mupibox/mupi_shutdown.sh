76f8c2e6f7029276c3322ffbc7123cfbe01d7810
splitti <45425109+splitti@users.noreply.github.com>
2022-02-27 01:02:34 +0100

    Update mupi_shutdown.sh

--- scripts/mupibox/mupi_shutdown.sh
+++ scripts/mupibox/mupi_shutdown.sh
@@ -8,7 +8,9 @@ CONFIG="/etc/mupibox/mupiboxconfig.json"
 SHUT_SOUND=$(/usr/bin/jq -r .mupibox.shutSound ${CONFIG})
 SHUT_SPLASH=$(/usr/bin/jq -r .mupibox.shutSplash ${CONFIG})
 AUDIO_DEVICE=$(/usr/bin/jq -r .mupibox.audioDevice ${CONFIG})
+START_VOLUME=$(/usr/bin/jq -r .mupibox.startVolume ${CONFIG})
 
 /usr/bin/fbv ${SHUT_SPLASH}
 sudo /usr/local/bin/mupibox/./setting_update.sh
-/usr/bin/mplayer ${SHUT_SOUND}
+su - dietpi -c '/usr/bin/amixer sset ${AUDIO_DEVICE} ${START_VOLUME}%'
+su - dietpi -c '/usr/bin/mplayer ${SHUT_SOUND}'
