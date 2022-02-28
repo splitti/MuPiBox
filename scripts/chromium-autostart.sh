b8c9dfb7f86b65b517a5ca621355432e579ead45
nero0345 <nero.erik@googlemail.com>
2022-02-28 18:56:01 +0100

    clean chrome

--- scripts/chromium-autostart.sh
+++ scripts/chromium-autostart.sh
@@ -10,7 +10,6 @@
 CONFIG="/etc/mupibox/mupiboxconfig.json"
 RES_X=$(/usr/bin/jq -r .chromium.resX ${CONFIG})
 RES_Y=$(/usr/bin/jq -r .chromium.resY ${CONFIG})
-SONOS_NETWORK="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/network.json"
 
 CHROMIUM_OPTS="--use-gl=egl --kiosk --test-type --window-size=${RES_X:-1280},${RES_Y:-720} --start-fullscreen --start-maximized --window-position=0,0"
 # If you want tablet mode, uncomment the next line.
@@ -22,11 +21,6 @@ URL="http://$(/usr/bin/jq -r .mupibox.host ${CONFIG}):8200"
 START_VOLUME=$(/usr/bin/jq -r .mupibox.startVolume ${CONFIG})
 AUDIO_DEVICE=$(/usr/bin/jq -r .mupibox.audioDevice ${CONFIG})
 /usr/bin/amixer sset ${AUDIO_DEVICE} ${START_VOLUME}%
-
-currentIP=$(hostname -I)
-/usr/bin/cat <<< $(/usr/bin/jq --arg v "${currentIP}" '.[].ip = $v' ${SONOS_NETWORK}) >  ${SONOS_NETWORK}
-currentHost=$(hostname)
-/usr/bin/cat <<< $(/usr/bin/jq --arg v "${currentHost}" '.[].host = $v' ${SONOS_NETWORK}) >  ${SONOS_NETWORK}
 
 # RPi or Debian Chromium package
 FP_CHROMIUM=$(command -v chromium-browser)
