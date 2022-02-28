79378273e5169d65b97d306af3592a8fa691366e
splitti <45425109+splitti@users.noreply.github.com>
2022-03-01 00:35:00 +0100

    Update

---
+++ scripts/mupibox/shutdown_sound.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+
+CONFIG="/etc/mupibox/mupiboxconfig.json"
+SHUT_SOUND=$(/usr/bin/jq -r .mupibox.shutSound ${CONFIG})
+AUDIO_DEVICE=$(/usr/bin/jq -r .mupibox.audioDevice ${CONFIG})
+START_VOLUME=$(/usr/bin/jq -r .mupibox.startVolume ${CONFIG})
+
+/usr/bin/amixer sset ${AUDIO_DEVICE} ${START_VOLUME}% 
+/usr/bin/mplayer ${SHUT_SOUND}
