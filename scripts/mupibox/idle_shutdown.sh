c48b2ac77faf2f34c66fe210806fa68031cb3b85
splitti <45425109+splitti@users.noreply.github.com>
2022-03-03 00:20:10 +0100

    0.0.7 idleTimer

--- scripts/mupibox/idle_shutdown.sh
+++ scripts/mupibox/idle_shutdown.sh
@@ -15,26 +15,27 @@ echo "$(date +'%d/%m/%Y %H:%M:%S')  # SERVICE STARTED" >> ${LOG}
 while true
 do
   sleep 10
-  max_idle_time=`/usr/bin/jq -r .timeout.idlePiShutdown ${CONFIG}` 
+  max_idle_time=$(/usr/bin/jq -r .timeout.idlePiShutdown ${CONFIG})
   if (($max_idle_time > 0))
   then
     if [[ $(head -n1 ${PLAYERSTATE}) != "play" ]]
     then
-      ((current_idle_time++))
-      if ((${current_idle_time} > ${max_idle_time}))
-      then
-        echo "$(date +'%d/%m/%Y %H:%M:%S')  # CURRENT IDLE TIME = ${current_idle_time}" >> ${LOG}  
-        echo "$(date +'%d/%m/%Y %H:%M:%S')  # MAX IDLE TIME REACHED - SHUTDOWN NOW" >> ${LOG}
+		((current_idle_time++))
+		idle=$(( current_idle_time / 6 ))
+		if ((${idle} > ${max_idle_time}))
+		then
+			echo "$(date +'%d/%m/%Y %H:%M:%S')  # CURRENT IDLE TIME = ${current_idle_time}" >> ${LOG}
+			echo "$(date +'%d/%m/%Y %H:%M:%S')  # MAX IDLE TIME REACHED - SHUTDOWN NOW" >> ${LOG}
 
-        # OnOffShim controlled shutdown
-        /usr/local/bin/mupibox/./software_shutdown.sh
-      fi
+			# OnOffShim controlled shutdown
+			/usr/local/bin/mupibox/./software_shutdown.sh
+		  fi
     else
-      current_idle_time=0
-	  echo "$(date +'%d/%m/%Y %H:%M:%S')  # CURRENT IDLE TIME = 0" >> ${LOG}
+		current_idle_time=0
+		echo "$(date +'%d/%m/%Y %H:%M:%S')  # CURRENT IDLE TIME = 0" >> ${LOG}
     fi
   else
-    current_idle_time=0
+		current_idle_time=0
   fi
   echo "$(date +'%d/%m/%Y %H:%M:%S')  # CURRENT IDLE TIME = ${current_idle_time}" >> ${LOG}
 done
