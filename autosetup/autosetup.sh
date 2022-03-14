bad7e01b69815bc7e73fae1edd6354205e690239
splitti <45425109+splitti@users.noreply.github.com>
2022-03-14 10:06:26 +0100

    Update autosetup.sh

--- autosetup/autosetup.sh
+++ autosetup/autosetup.sh
@@ -22,7 +22,7 @@ exec 3>${LOG}
 	echo -e "XXX\n0\nInstall some packages... Please wait!\nXXX"
 	# Get missing packages
 	sudo apt-get update >&3 2>&3
-	packages2install = "git libasound2 jq samba mplayer pulseaudio-module-bluetooth bluez zip rrdtools scrot"
+	packages2install="git libasound2 jq samba mplayer pulseaudio-module-bluetooth bluez zip rrdtools scrot"
 	sudo apt-get install ${packages2install} -y >&3 2>&3
 
 	for thispackage in `echo ${packages2install}`; do
