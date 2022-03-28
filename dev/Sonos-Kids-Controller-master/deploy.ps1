30043bc0d7541ddac4178b7403de4120b0b85a88
splitti <45425109+splitti@users.noreply.github.com>
2022-03-29 00:22:05 +0200

    Add Powershell Deploy

---
+++ dev/Sonos-Kids-Controller-master/deploy.ps1
@@ -0,0 +1,19 @@
+﻿
+remove-item .\deploy -Recurse -Confirm:$false
+Remove-Item .\deploy.zip -Confirm:$false
+
+# build personal web app
+ionic build --prod
+
+# copy everything to deploy directory
+mkdir deploy
+
+Copy-Item www .\deploy\ -Recurse -Confirm:$false
+Copy-Item server .\deploy\ -Recurse -Confirm:$false
+
+Copy-Item server.js .\deploy\ -Confirm:$false
+Copy-Item  package-deploy.json .\deploy\package.json -Confirm:$false
+Copy-Item README.md .\deploy\ -Confirm:$false
+
+# archive
+Compress-Archive -Path .\deploy\* -DestinationPath .\deploy.zip
