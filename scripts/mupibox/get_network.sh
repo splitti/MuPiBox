1bf64a240d725d2d2ee8eed4821e7a115c64afab
splitti <45425109+splitti@users.noreply.github.com>
2022-04-12 01:33:03 +0200

    Update get_network.sh

--- scripts/mupibox/get_network.sh
+++ scripts/mupibox/get_network.sh
@@ -1,15 +1,30 @@
+
 #!/bin/bash
 #
-# Get IP Address
+# Get Network-Data and create Online / Offline Data.json for showing Media in MuPiBox
 
+DATA_FILE="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
+ACTIVE_FILE="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/active_data.json"
+OFFLINE_FILE="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/offline_data.json"
 CONFIG="/etc/mupibox/mupiboxconfig.json"
 NETWORKCONFIG="/tmp/network.json"
-PLAYERSTATE=$(cat /tmp/playerstate)
+#PLAYERSTATE=$(cat /tmp/playerstate)
 
 if [[ ! -f ${NETWORKCONFIG} ]]; then
-        sudo touch ${NETWORKCONFIG}
+        sudo echo "{}" > ${NETWORKCONFIG}
         sudo chown dietpi:dietpi ${NETWORKCONFIG}
         sudo chmod 777 ${NETWORKCONFIG}
+        OLD_ONLINESTATE="starting"
+else
+        OLD_ONLINESTATE=$(/usr/bin/jq -r .onlinestate ${NETWORKCONFIG})
+fi
+
+if [[ ! -f ${OFFLINE_FILE} ]]; then
+        sudo echo "{}" > ${OFFLINE_FILE}
+        sudo chown dietpi:dietpi ${OFFLINE_FILE}
+        echo $(jq '.[] | select(.type != "spotify") | select(.type != "tunein")' < ${DATA_FILE}) > ${OFFLINE_FILE}
+elif [ $(stat --format='%Y' "${DATA_FILE}") -gt $(stat --format='%Y' "${OFFLINE_FILE}") ]; then
+        echo $(jq '.[] | select(.type != "spotify") | select(.type != "tunein")' < ${DATA_FILE}) > ${OFFLINE_FILE}
 fi
 
 GW=$(ip route show 0.0.0.0/0 dev wlan0 | cut -d\  -f3)
@@ -22,7 +37,7 @@ IPA=$(/usr/bin/hostname -I | awk '{print $1}')
 DNS=$(echo $(sudo cat /etc/resolv.conf | grep 'nameserver ') | sed 's/nameserver //g')
 SUBNET=$(/sbin/ifconfig wlan0 | awk '/netmask/{split($4,a,":"); print a[1]}')
 
-/usr/bin/cat <<< $(/usr/bin/jq -n --arg v "${HOSTN}" '.host = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
+/usr/bin/cat <<< $(/usr/bin/jq --arg v "${HOSTN}" '.host = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${IPA}" '.ip = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${MAC}" '.mac = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${WIFI}" '.wifi = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
@@ -32,13 +47,22 @@ SUBNET=$(/sbin/ifconfig wlan0 | awk '/netmask/{split($4,a,":"); print a[1]}')
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${DNS}" '.dns = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${SUBNET}" '.subnet = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
 
-#if [[ ${PLAYERSTATE} != "play" ]]; then
-        if [[ $(ping -c3 -4 -w 4 1.1.1.1) ]]; then
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
                 ONLINESTATE="online"
+                if [[ ${OLD_ONLINESTATE} != "online" ]]; then
+                        sudo rm ${ACTIVE_FILE}
+                        sudo ln -s ${DATA_FILE} ${ACTIVE_FILE}
+                        sudo chown dietpi:dietpi ${ACTIVE_FILE}
+                fi
         else
                 ONLINESTATE="offline"
-        fi
-#else
-#        ONLINESTATE="playing"
-#fi
+                if [[ ${OLD_ONLINESTATE} != "offline" ]]; then
+                        sudo rm ${ACTIVE_FILE}
+                        sudo ln -s ${OFFLINE_FILE} ${ACTIVE_FILE}
+                        sudo chown dietpi:dietpi ${ACTIVE_FILE}
+                fi
+fi
+
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${ONLINESTATE}" '.onlinestate = $v' ${NETWORKCONFIG}) >  ${NETWORKCONFIG}
