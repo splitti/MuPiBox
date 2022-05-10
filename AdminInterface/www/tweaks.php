a55e9dacc16841efacbe77367a159a4f935ebf4c
Olaf Splitt <olaf.splitt@gmail.com>
2022-05-11 01:14:42 +0200

    0.0.124

--- AdminInterface/www/tweaks.php
+++ AdminInterface/www/tweaks.php
@@ -20,6 +20,45 @@
                 $change=1;
                 $CHANGE_TXT=$CHANGE_TXT."<li>Wait for Network on boot is disabled</li>";
                 }
+  if( $_POST['change_turbo'] == "disable" )
+        {
+        $command = "sudo su - dietpi -c \". /boot/dietpi/func/dietpi-globals && G_SUDO G_CONFIG_INJECT 'initial_turbo' 'initial_turbo=0' /boot/config.txt\"";
+        exec($command, $output, $result );
+        $change=1;
+        $CHANGE_TXT=$CHANGE_TXT."<li>Inital Turbo disabled</li>";
+        }
+  else if( $_POST['change_turbo'] == "enable" )
+        {
+        $command = "sudo su - dietpi -c \". /boot/dietpi/func/dietpi-globals && G_SUDO G_CONFIG_INJECT 'initial_turbo' 'initial_turbo=30' /boot/config.txt\"";
+        exec($command, $output, $result );
+        $change=1;
+        $CHANGE_TXT=$CHANGE_TXT."<li>Inital Turbo enabled</li>";
+        }
+
+  if( $_POST['change_swap'] == "disable" )
+        {
+        $command = "sudo /boot/dietpi/func/dietpi-set_swapfile 0";
+        exec($command, $output, $result );
+        $change=1;
+        $CHANGE_TXT=$CHANGE_TXT."<li>SWAP disabled</li>";
+        }
+  else if( $_POST['change_swap'] == "enable" )
+        {
+        $command = "sudo /boot/dietpi/func/dietpi-set_swapfile 1";
+        exec($command, $output, $result );
+        $change=1;
+        $CHANGE_TXT=$CHANGE_TXT."<li>SWAP enabled</li>";
+        }
+
+  if ($_POST['change_cpug'])
+   {
+   $command = "sudo su - dietpi -c \". /boot/dietpi/func/dietpi-globals && G_SUDO G_CONFIG_INJECT 'CONFIG_CPU_GOVERNOR=' 'CONFIG_CPU_GOVERNOR=".$_POST['cpugovernor']."' /boot/dietpi.txt\"";
+   $test=exec($command, $output, $result );
+   $command = "sudo /boot/dietpi/func/dietpi-set_cpu";
+   exec($command, $output, $result );
+   $change=1;
+   $CHANGE_TXT=$CHANGE_TXT."<li>CPU Governor changet to  ".$_POST['cpugovernor']."</li>";
+   }
 
         if( $_POST['change_sd'] == "activate for next boot" )
                 {
@@ -72,30 +111,108 @@
                 </div>
                         <ul >
 
-                                                                <li class="li_1"><h2>Overclock SD Card</h2>
-                                                                <p>
-                                                                Just for highspeed SD Cards. You can damage data or the microSD itself!
-                                                                </p>
-                                                                <p>
-                                                                <?php
-                                                                echo "Overclocking state: <b>".$sd_state."</b>";
+				<li class="li_1"><h2>Overclock SD Card</h2>
+				<p>
+				Just for highspeed SD Cards. You can damage data or the microSD itself!
+				</p>
+				<p>
+				<?php
+				echo "Overclocking state: <b>".$sd_state."</b>";
+				?>
+				</p>
+				<input id="saveForm" class="button_text" type="submit" name="change_sd" value="<?php print $change_sd; ?>" />
+		</li>
+
+				<li class="li_1"><h2>Wait for Network on boot</h2>
+				<p>
+				Speeds up the boot time, but sometimes the boot process is to fast and you have to wait for the network to be ready... Try it, if disabling this option works for you!
+				</p>
+				<p>
+				<?php
+				echo "Wait for Network on boot: <b>".$netboot_state."</b>";
+				?>
+				</p>
+				<input id="saveForm" class="button_text" type="submit" name="change_netboot" value="<?php print $change_netboot; ?>" />
+		</li>
+
+				<li class="li_1"><h2>Initial Turbo</h2>
+				<p>
+				Initial Turbo avoids throtteling sometimes...
+				</p>
+				<p>
+				<?php
+				$command = "cat /boot/config.txt | grep initial_turbo | cut -d '=' -f 2";
+				$turbo = exec($command, $output);
+				echo "Turbo seconds: <b>".$turbo."</b>";
+                if($turbo == 0)
+                {
+                        $change_turbo="enable";
+                }
+                else
+                {
+                        $change_turbo="disable";
+                }
+
+					?>
+					</p>
+				<input id="saveForm" class="button_text" type="submit" name="change_turbo" value="<?php print $change_turbo; ?>" />
+
+			</li>
+
+
+					<li class="li_1"><h2>CPU Governor</h2>
+					<p>
+					Limits CPU frequency to 600 MHz. Helps to avoid throtteling.
+					</p>
+					<p>
+                                <div>
+                        <select id="cpugovernor" name="cpugovernor" class="element text medium">
+<?php
+ $command = "cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors";
+ $governors = exec($command, $output);
+ $cpug = explode(" ", $governors);
+ $command = "cat /boot/dietpi.txt | grep CONFIG_CPU_GOVERNOR | cut -d '=' -f 2";
+ $current_governor = exec($command, $output);
+
+      foreach($cpug as $key) {
+       if( $key == $current_governor )
+        {
+        $selected = " selected=\"selected\"";
+        }
+       else
+        {
+        $selected = "";
+        }
+       print "<option value=\"". $key . "\"" . $selected  . ">" . $key . "</option>";
+      }
+?>
+"</select>
+                </div>
+				</p>
+				<input id="saveForm" class="button_text" type="submit" name="change_cpug" value="Save CPU Governor" />
+		</li>
+				<li class="li_1"><h2>SWAP</h2>
+				<p>
+				Enables or disables SWAP!
+				</p>
+				<p>
+				<?php
+                $command = "cat /boot/dietpi.txt | grep AUTO_SETUP_SWAPFILE_SIZE= | cut -d '=' -f 2";
+                $currentswapsize = exec($command, $output);
+                if($currentswapsize == 0)
+                {
+                        $change_swap="enable";
+                }
+                else
+                {
+                        $change_swap="disable";
+                }
+
+                                                                echo "SWAP Size: <b>".$currentswapsize." MB</b>";
                                                                 ?>
                                                                 </p>
-                                                                <input id="saveForm" class="button_text" type="submit" name="change_sd" value="<?php print $change_sd; ?>" />
+                                                                <input id="saveForm" class="button_text" type="submit" name="change_swap" value="<?php print $change_swap; ?>" />
                                                         </li>
-
-                                                                <li class="li_1"><h2>Wait for Network on boot</h2>
-                                                                <p>
-                                                                Speeds up the boot time, but sometimes the boot process is to fast and you have to wait for the network to be ready... Try it, if disabling this option works for you!
-                                                                </p>
-                                                                <p>
-                                                                <?php
-                                                                echo "Wait for Network on boot: <b>".$netboot_state."</b>";
-                                                                ?>
-                                                                </p>
-                                                                <input id="saveForm" class="button_text" type="submit" name="change_netboot" value="<?php print $change_netboot; ?>" />
-                                                        </li>
-
 
                         </ul>
                 </form>
