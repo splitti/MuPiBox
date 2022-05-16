cf8c9c32eb40e9431f71efac55946bd5f52f6ef6
splitti <45425109+splitti@users.noreply.github.com>
2022-05-16 12:25:46 +0200

    Update m3u_generator.sh

--- scripts/mupibox/m3u_generator.sh
+++ scripts/mupibox/m3u_generator.sh
@@ -2,77 +2,42 @@
 #
 # Generate M3U Playlist and Covers
 
-SAVEIFS=$IFS
-IFS=$(echo -en "\n\b")
-
 DATA="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
 TYPE="library"
-element=0
 
-for topFolder in /home/dietpi/MuPiBox/media/audiobook/* ; do
-	artist=$(/usr/bin/basename "${topFolder}")
-	for i in ${topFolder}/* ; do
-		if [ ${#i} -gt 40 ]
-		then
-			dirname=$(/usr/bin/basename "${i}")
-			if [ ${#dirname} -gt 0 ] && [ ${#i} -gt ${#topFolder} ]
-			then
-				setCover=0
-				ls -1v "${i}" | grep '.mp3\|.flac\|.wav\|.wma' > /tmp/playlist.m3u && mv /tmp/playlist.m3u "${i}"
-				if [ -f "${i}"/*.jp*g ]
-				then
-					/usr/bin/mkdir -p "/home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/audiobook/${artist}/${dirname}/" > /dev/null
-					/usr/bin/cp --update "${i}"/*.jp*g "/home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/audiobook/${artist}/${dirname}/cover.jpg"
-					setCover=1
-				fi
-				searchStr=`/usr/bin/cat "${DATA}" | grep 'audiobook/'"${artist}"'/'"${dirname}"'/cover.jpg'`
-				if [ -z "${searchStr}" ]
-				then
-					if [ $setCover == 1 ]
-					then
-						/usr/bin/cat <<< $(cat ${DATA} | /usr/bin/jq '. += [{"type": "library", "category": "audiobook", "artist": "'"${artist}"'", "title": "'"${dirname}"'", "cover": "http://127.0.0.1:8200/cover/audiobook/'"${artist}"'/'"${dirname}"'/cover.jpg"}]') > ${DATA}
-					else
-						/usr/bin/cat <<< $(cat ${DATA} | /usr/bin/jq '. += [{"type": "library", "category": "audiobook", "artist": "'"${artist}"'", "title": "'"${dirname}"'"}]') > ${DATA}
-					fi
-				fi
-			fi
-		fi
-	done
-done
-
-for topFolder in /home/dietpi/MuPiBox/media/music/* ; do
-	artist=$(/usr/bin/basename "${topFolder}")
-	for i in ${topFolder}/* ; do
-		if [ ${#i} -gt 37 ]
-		then
-			dirname=$(/usr/bin/basename "${i/${topFolder}/}")
-			if [ ${#dirname} -gt 0 ] && [ ${#i} -gt ${#topFolder} ]
-			then
-				setCover=0
-				ls -1v "${i}" | grep '.mp3\|.flac\|.wav\|.wma' > /tmp/playlist.m3u && mv /tmp/playlist.m3u "${i}"
-				if [ -f "${i}"/*.jp*g ]
-				then
-					/usr/bin/mkdir -p "/home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/music/${artist}/${dirname}/" > /dev/null
-					/usr/bin/cp --update "${i}"/*.jp*g "/home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/music/${artist}/${dirname}/cover.jpg"
-					setCover=1
-				fi
-				searchStr=`/usr/bin/cat "${DATA}" | grep 'music/'"${artist}"'/'"${dirname}"'/cover.jpg'`
-				if [ -z "${searchStr}" ]
-				then
-					if [ $setCover == 1 ]
-					then
-						/usr/bin/cat <<< $(cat ${DATA} | /usr/bin/jq '. += [{"type": "library", "category": "music", "artist": "'"${artist}"'", "title": "'"${dirname}"'", "cover": "http://127.0.0.1:8200/cover/music/'"${artist}"'/'"${dirname}"'/cover.jpg"}]') > ${DATA}
-					else
-						/usr/bin/cat <<< $(cat ${DATA} | /usr/bin/jq '. += [{"type": "library", "category": "music", "artist": "'"${artist}"'", "title": "'"${dirname}"'"}]') > ${DATA}
-					fi
-				fi
-			fi
-		fi
-	done
+for topFolder in "/home/dietpi/MuPiBox/media/music/"* ; do
+        artist=$(/usr/bin/basename "${topFolder}")
+        for i in "${topFolder}/"* ; do
+                if [ ${#i} -gt 36 ]
+                then
+                        title=$(/usr/bin/basename "${i}")
+                        if [ "${title}" != "*" ] # && [ ${#i} -gt ${#topFolder} ]
+                        then
+                                setCover=0
+                                ls -1v "${i}" | grep '.mp3\|.flac\|.wav\|.wma' > /tmp/playlist.m3u && mv /tmp/playlist.m3u "${i}"
+                                if [ -f "${i}"/*.jp*g ]
+                                then
+                                        /usr/bin/mkdir -p "/home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/music/${artist}/${title}/" > /dev/null
+                                        /usr/bin/cp --update "${i}"/*.jp*g "/home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/music/${artist}/${title}/cover.jpg"
+                                        setCover=1
+                                fi
+                                searchStr=`/usr/bin/cat ${DATA} | grep 'music/'"${artist}"'/'"${title}"'/cover.jpg'`
+                                if [ -z "${searchStr}" ]
+                                then
+									if [ $setCover == 1 ]
+									then
+										/usr/bin/cat <<< $(/usr/bin/cat ${DATA} | /usr/bin/jq '. += [{"type": "library", "category": "music", "artist": "'"${artist}"'", "title": "'"${title}"'", "cover": "http://127.0.0.1:8200/cover/music/'"${artist}"'/'"${title}"'/cover.jpg"}]') > ${DATA}
+									else
+										/usr/bin/cat <<< $(/usr/bin/cat ${DATA} | /usr/bin/jq '. += [{"type": "library", "category": "music", "artist": "'"${artist}"'", "title": "'"${title}"'"}]') > ${DATA}
+									fi
+                                fi
+                        fi
+                fi
+        done
 done
 
 /usr/bin/chown -R dietpi:dietpi /home/dietpi/MuPiBox/media/
 /usr/bin/chown -R dietpi:dietpi /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/cover/
-echo "Cover & Playlists generated"
+/usr/bin/chown dietpi:dietpi ${DATA}
 
-IFS=${SAVEIFS}
+echo "Cover & Playlists generated"
