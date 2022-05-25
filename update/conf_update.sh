a5f4ce45d188aec5f38de20dd8d5728cc0e01f1b
splitti <45425109+splitti@users.noreply.github.com>
2022-05-25 13:00:38 +0200

    1.0.8

---
+++ update/conf_update.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+#
+
+#https://raw.githubusercontent.com/splitti/MuPiBox/main
+SRC="https://mupibox.de/version/latest"
+CONFIG="/etc/mupibox/mupiboxconfig.json"
+LOG="/tmp/mupibox-conf_update.log"
+exec 3>${LOG}
+
+
+{
+	echo -e "XXX\n0\nRemove old values... \nXXX"	 >&3 2>&3
+
+	/usr/bin/cat <<< $(/usr/bin/jq 'del(.mupibox.googlettslanguages)' ${CONFIG}) > ${CONFIG}
+	mv ${LOG} /home/dietpi/.mupibox/last_update.log >&3 2>&3
+	chown -R dietpi:dietpi /home/dietpi/.mupibox/last_update.log >&3 2>&3
+
+} | whiptail --title "MuPiBox-Conf Update" --gauge "Please wait while installing" 6 60 0
