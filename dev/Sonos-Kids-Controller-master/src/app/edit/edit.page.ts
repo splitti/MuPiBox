dfe22ee40aa1712592d02065cbe358d00e9004ba
nero0345 <nero.erik@googlemail.com>
2022-06-01 12:32:04 +0200

    test

--- dev/Sonos-Kids-Controller-master/src/app/edit/edit.page.ts
+++ dev/Sonos-Kids-Controller-master/src/app/edit/edit.page.ts
@@ -47,10 +47,10 @@ export class EditPage implements OnInit {
           text: 'Ok',
           handler: () => {
             this.mediaService.deleteRawMediaAtIndex(index);
-            window.setTimeout(() => {
-            }, 1000);
-            this.media = this.mediaService.getRawMediaObservable();
-            this.mediaService.updateRawMedia();
+            setTimeout(() => {
+              this.media = this.mediaService.getRawMediaObservable();
+              this.mediaService.updateRawMedia();
+            }, 1000)
           }
         },
         {
