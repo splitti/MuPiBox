a1cc34d8f8f50f42c68e7dc53f2acdbea312274d
nero0345 <nero.erik@googlemail.com>
2022-06-07 02:01:43 +0200

    test

--- dev/Sonos-Kids-Controller-master/src/app/current.playlist.ts
+++ dev/Sonos-Kids-Controller-master/src/app/current.playlist.ts
@@ -1,13 +1,11 @@
 export interface CurrentPlaylist {
     total?: number;
-    items: SpotifyItem[];
-}
-
-export interface SpotifyItem {
-    track:{
-        duration_ms: number;
-        id: number;
-        name: string;
-        type: string;
+    [items: number]:{
+        track:{
+            duration_ms: number;
+            id: number;
+            name: string;
+            type: string;
+        }
     }
 }
