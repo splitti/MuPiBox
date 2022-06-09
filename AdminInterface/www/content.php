2c38075866ed2d764f408921cb4341f4918212ec
Olaf Splitt <olaf.splitt@gmail.com>
2022-06-09 19:24:27 +0200

    1.0.10

--- AdminInterface/www/content.php
+++ AdminInterface/www/content.php
@@ -5,6 +5,7 @@
 <h2>MuPiBox</h2>
 <p>You can add Music, Streams or control the box here...</p>
 <?php
+        $ip=exec("hostname -I | awk '{print $1}'");
         print "<p><embed src='http://".$ip.":8200' id='remotecontrol'></p>";
         print "<p><a href='http://".$ip.":8200' id='remotecontrol' target='_blank'>If it doesn't display properly or can't be served, try this Link and click me...</a></p>";
 
