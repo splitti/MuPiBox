584e4def3eb388ae4bb2e8350f7c27de637bd9fa
splitti <45425109+splitti@users.noreply.github.com>
2022-09-16 21:49:53 +0200

    Bugfix 1.1.1
    Too much arguments because of to much audio files

--- scripts/mupibox/add_index.sh
+++ scripts/mupibox/add_index.sh
@@ -2,6 +2,9 @@
 #
 
 CONFIG="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
+CONFIG_TMP="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
 
-/usr/bin/echo "$(/usr/bin/cat ${CONFIG} | grep -v '        "index":')" > ${CONFIG}
-/usr/bin/echo "$(/usr/bin/perl -pe 'BEGIN{$k=-1};s/{/$& . "\n        \"index\": " .  ++$k . ","/e' ${CONFIG})" > ${CONFIG}
+
+/usr/bin/cat ${CONFIG} | grep -v '        "index":' > ${CONFIG_TMP}
+/usr/bin/perl -pe 'BEGIN{$k=-1};s/{/$& . "\n        \"index\": " .  ++$k . ","/e' ${CONFIG_TMP} > ${CONFIG}
+rm ${CONFIG_TMP}
