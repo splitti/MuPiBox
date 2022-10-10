15ca2c85217e584ee50f1f32d0ba5bf6227a3548
nero0345 <nero.erik@googlemail.com>
2022-10-10 23:10:48 +0200

    Bugfix remove data.json

--- scripts/mupibox/add_index.sh
+++ scripts/mupibox/add_index.sh
@@ -2,7 +2,7 @@
 #
 
 CONFIG="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
-CONFIG_TMP="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
+CONFIG_TMP="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data_tmp.json"
 
 
 /usr/bin/cat ${CONFIG} | grep -v '        "index":' > ${CONFIG_TMP}
