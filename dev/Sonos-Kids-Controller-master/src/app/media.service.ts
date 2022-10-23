8bbc54dc4a123e37bdcfd9245b75109a2b25ae4f
nero0345 <nero.erik@googlemail.com>
2022-10-24 01:26:58 +0200

    test

--- dev/Sonos-Kids-Controller-master/src/app/media.service.ts
+++ dev/Sonos-Kids-Controller-master/src/app/media.service.ts
@@ -241,7 +241,7 @@ export class MediaService {
               })
             ),
             iif(
-              () => (item.showid && item.showid.length > 0 && item.title.length == 0) ? true : false, // Get media by show
+              () => (item.showid && item.showid.length > 0 && !item.title) ? true : false, // Get media by show
                 this.spotifyService.getMediaByShowID(item.showid, item.category, item.index, item.shuffle).pipe(
                   map(items => {  // If the user entered an user-defined artist name in addition to a query, overwrite orignal artist from spotify
                     if (item.artist?.length > 0) {
