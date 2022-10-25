850e873f916dd2ac6f62a755179da8f8f5e8094b
alex-vg <22611767+alex-vg@users.noreply.github.com>
2022-10-25 20:17:22 +0200

    added SpotifyWebApi imports

--- dev/Sonos-Kids-Controller-master/src/app/spotify.service.ts
+++ dev/Sonos-Kids-Controller-master/src/app/spotify.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Observable, defer, throwError, of, range } from 'rxjs';
-import { retryWhen, flatMap, tap, delay, take, map, mergeMap, mergeAll, toArray } from 'rxjs/operators';
+import { retryWhen, tap, delay, take, map, mergeMap, mergeAll, toArray } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
-import { SpotifyAlbumsResponse, SpotifyAlbumsResponseItem, SpotifyArtistsAlbumsResponse, SpotifyEpisodeResponse, SpotifyShowResponse } from './spotify';
+import { SpotifyAlbumsResponseItem } from './spotify';
 import { Media } from './media';
-
-declare const require: any;
+import { default as SpotifyWebApi } from "spotify-web-api-js";
+import SpotifyWebApiJs = SpotifyWebApi.SpotifyWebApiJs;
+import AlbumSearchResponse = SpotifyApi.AlbumSearchResponse;
+import ArtistsAlbumsResponse = SpotifyApi.ArtistsAlbumsResponse;
+import AlbumObjectSimplified = SpotifyApi.AlbumObjectSimplified;
+import PagingObject = SpotifyApi.PagingObject;
+import SingleShowResponse = SpotifyApi.SingleShowResponse;
+import ShowEpisodesResponse = SpotifyApi.ShowEpisodesResponse;
+import EpisodeObjectSimplified = SpotifyApi.EpisodeObjectSimplified;
 
 @Injectable({
   providedIn: 'root'
 })
 export class SpotifyService {
 
-  spotifyApi: any;
   refreshingToken = false;
-
+  private spotifyApi: SpotifyWebApiJs;
   constructor(private http: HttpClient) {
-    const SpotifyWebApi = require('../../src/app/spotify-web-api.js');
-    this.spotifyApi = new SpotifyWebApi();
+    this.spotifyApi = new SpotifyWebApi()
   }
 
   getMediaByQuery(query: string, category: string, index: number, shuffle: boolean): Observable<Media[]> {
@@ -26,17 +31,17 @@ export class SpotifyService {
       retryWhen(errors => {
         return this.errorHandler(errors);
       }),
-      map((response: SpotifyAlbumsResponse) => response.albums.total),
+      map((response: AlbumSearchResponse) => response.albums.total),
       mergeMap(count => range(0, Math.ceil(count / 50))),
       mergeMap(multiplier => defer(() => this.spotifyApi.searchAlbums(query, { limit: 50, offset: 50 * multiplier, market: 'DE' })).pipe(
         retryWhen(errors => {
           return this.errorHandler(errors);
         }),
-        map((response: SpotifyAlbumsResponse) => {
+        map((response: AlbumSearchResponse) => {
           return response.albums.items.map(item => {
             const media: Media = {
               id: item.id,
-              artist: item.artists[0].name,
+              artist: item.artists[0].name, // TS2339: Property 'artists' does not exist on type 'AlbumObjectSimplified'.
               title: item.name,
               cover: item.images[0].url,
               type: 'spotify',
@@ -60,17 +65,17 @@ export class SpotifyService {
       retryWhen(errors => {
         return this.errorHandler(errors);
       }),
-      map((response: SpotifyArtistsAlbumsResponse) => response.total),
+      map((response: ArtistsAlbumsResponse) => response.total),
       mergeMap(count => range(0, Math.ceil(count / 50))),
       mergeMap(multiplier => defer(() => this.spotifyApi.getArtistAlbums(id, { include_groups: 'album', limit: 50, offset: 50 * multiplier, market: 'DE' })).pipe(
         retryWhen(errors => {
           return this.errorHandler(errors);
         }),
-        map((response: SpotifyArtistsAlbumsResponse) => {
+        map((response: PagingObject<AlbumObjectSimplified>) => {
           return response.items.map(item => {
             const media: Media = {
               id: item.id,
-              artist: item.artists[0].name,
+              artist: item.artists[0].name, // TS2339: Property 'artists' does not exist on type 'AlbumObjectSimplified'.
               title: item.name,
               cover: item.images[0].url,
               type: 'spotify',
@@ -94,20 +99,20 @@ export class SpotifyService {
       retryWhen(errors => {
         return this.errorHandler(errors);
       }),
-      map((response: SpotifyShowResponse) => {return response.name}),
+      map((response: SingleShowResponse) => {return response.name}),
       toArray()
     );
     const albums = defer(() => this.spotifyApi.getShow(id, { limit: 1, offset: 0, market: 'DE' })).pipe(
       retryWhen(errors => {
         return this.errorHandler(errors);
       }),
-      map((response: SpotifyShowResponse) => response.episodes.total),
+      map((response: SpotifyApi.SingleShowResponse) => response.episodes.total),
       mergeMap(count => range(0, Math.ceil(count / 50))),
       mergeMap(multiplier => defer(() => this.spotifyApi.getShowEpisodes(id, { limit: 50, offset: 50 * multiplier, market: 'DE' })).pipe(
         retryWhen(errors => {
           return this.errorHandler(errors);
         }),
-        map((response: SpotifyEpisodeResponse) => {
+        map((response: SpotifyApi.ShowEpisodesResponse) => {
           return response.items.map(item => {
             const media: Media = {
               showid: item.id,
@@ -136,13 +141,13 @@ export class SpotifyService {
       retryWhen(errors => {
         return this.errorHandler(errors);
       }),
-      map((response: SpotifyEpisodeResponse) => response.total),
+      map((response: ShowEpisodesResponse) => response.total),
       mergeMap(count => range(0, Math.ceil(count / 50))),
       mergeMap(multiplier => defer(() => this.spotifyApi.getShowEpisodes(id, { limit: 50, offset: 50 * multiplier, market: 'DE' })).pipe(
         retryWhen(errors => {
           return this.errorHandler(errors);
         }),
-        map((response: SpotifyEpisodeResponse) => {
+        map((response: PagingObject<EpisodeObjectSimplified>) => {
           return response.items.map(item => {
             const media: Media = {
               showid: item.id,
@@ -206,7 +211,7 @@ export class SpotifyService {
       retryWhen(errors => {
         return this.errorHandler(errors);
       }),
-      map((response: SpotifyAlbumsResponse) => {
+      map((response: AlbumSearchResponse) => {
         return response?.albums?.items?.[0]?.images?.[0]?.url || '';
       })
     );
@@ -225,7 +230,7 @@ export class SpotifyService {
 
   errorHandler(errors: Observable<any>) {
     return errors.pipe(
-      flatMap((error) => (error.status !== 401 && error.status !== 429) ? throwError(error) : of(error)),
+      mergeMap((error) => (error.status !== 401 && error.status !== 429) ? throwError(error) : of(error)),
       tap(_ => {
         if (!this.refreshingToken) {
           this.refreshToken();
