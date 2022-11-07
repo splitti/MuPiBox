251a74a78c315b6d511eb0216b4e85f504d66eef
Olaf Splitt <olaf.splitt@gmail.com>
2022-11-07 22:33:08 +0100

    Optimized add_wifi.sh
    Optimized by Vadder (User in Discord)

--- scripts/mupibox/add_wifi.sh
+++ scripts/mupibox/add_wifi.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 #
 
-
 MUPIWIFI="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/wlan.json"
 WPACONF="/etc/wpa_supplicant/wpa_supplicant.conf"
-
 
 while true
 do
 	if test -f "${MUPIWIFI}"; then
 		SSID=$(/usr/bin/jq -r .[].ssid ${MUPIWIFI})
 		PSK=$(/usr/bin/jq -r .[].pw ${MUPIWIFI})
-		cat << _EOF_ >> ${WPACONF}
-network={
-        ssid="${SSID}"
-        scan_ssid=1
-        key_mgmt=WPA-PSK
-        psk="${PSK}"
-}
+
+		if [ ${SSID} = "all" ] && [ ${PSK} = "clear"  ]
+		then
+			sudo rm ${WPACONF}
+			cat << _EOF_ >> ${WPACONF}
+# Grant all members of group "netdev" permissions to configure WiFi, e.g. via wpa_cli or wpa_gui
+ctrl_interface=DIR=/run/wpa_supplicant GROUP=netdev
+# Allow wpa_cli/wpa_gui to overwrite this config file
+update_config=1
+
+
 _EOF_
+		else
+			sudo -i wpa_passphrase "${SSID}" "${PSK}" >> ${WPACONF}
+		fi
+
 		rm ${MUPIWIFI}
 		sudo service ifup@wlan0 restart
 	fi
