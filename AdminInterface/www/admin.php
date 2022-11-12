dd1af90448d58b6f34fe3e92170cf6550f4a9b63
Olaf Splitt <olaf.splitt@gmail.com>
2022-11-12 01:28:52 +0100

    Bugfix

--- AdminInterface/www/admin.php
+++ AdminInterface/www/admin.php
@@ -158,10 +158,10 @@
 		}
 	if( $change == 1 )
 		{
-		$json_object = json_encode($data);
-		$save_rc = file_put_contents('/tmp/.mupiboxconfig.json', $json_object);
-		exec("sudo chmod 755 /etc/mupibox/mupiboxconfig.json");
-		exec("sudo mv /tmp/.mupiboxconfig.json /etc/mupibox/mupiboxconfig.json");
+		//$json_object = json_encode($data);
+		//$save_rc = file_put_contents('/tmp/.mupiboxconfig.json', $json_object);
+		//exec("sudo chmod 755 /etc/mupibox/mupiboxconfig.json");
+		//exec("sudo mv /tmp/.mupiboxconfig.json /etc/mupibox/mupiboxconfig.json");
 		exec("sudo /usr/local/bin/mupibox/./setting_update.sh");
 		exec("sudo -i -u dietpi /usr/local/bin/mupibox/./restart_kiosk.sh");
 		}
