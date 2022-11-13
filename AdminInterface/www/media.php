044023eb9ecabf6c38839c50481e3f6214703139
Olaf Splitt <olaf.splitt@gmail.com>
2022-11-13 22:02:17 +0100

    Adding Image Upload

--- AdminInterface/www/media.php
+++ AdminInterface/www/media.php
@@ -2,14 +2,8 @@
  include ('includes/header.php');
 ?>
 <div class="main">
-<h2>MuPiBox</h2>
-<p>You can add Music, Streams or control the box here...</p>
-<?php
-        $ip=exec("hostname -I | awk '{print $1}'");
-        print "<p><embed src='http://".$ip.":8200' id='remotecontrol'></p>";
-        print "<p><a href='http://".$ip.":8200' id='remotecontrol' target='_blank'>If it doesn't display properly or can't be served, try this Link and click me...</a></p>";
-
-?>
+<h2>MuPiBox - Media</h2>
+<p>Coming soon...</p>
 
 
 </div>
