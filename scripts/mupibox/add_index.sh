485bbffa41a121a82506d552cf976c1bc543d6d2
Olaf Splitt <olaf.splitt@gmail.com>
2022-11-16 22:28:59 +0100

    Bugfix JSON-Format

--- scripts/mupibox/add_index.sh
+++ scripts/mupibox/add_index.sh
@@ -2,9 +2,11 @@
 #
 
 CONFIG="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data.json"
-CONFIG_TMP="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/data_tmp.json"
+CONFIG_TMP="/tmp/data_tmp.json"
 
 
 /usr/bin/cat ${CONFIG} | grep -v '"index":' > ${CONFIG_TMP}
 /usr/bin/perl -pe 'BEGIN{$k=-1};s/{/$& . "\n        \"index\": " .  ++$k . ","/e' ${CONFIG_TMP} > ${CONFIG}
-rm ${CONFIG_TMP}
+/usr/bin/rm ${CONFIG_TMP}
+/usr/bin/echo $(/usr/bin/jq -c . ${CONFIG}) | /usr/bin/jq . > ${CONFIG_TMP}
+/usr/bin/mv ${CONFIG_TMP} ${CONFIG}
