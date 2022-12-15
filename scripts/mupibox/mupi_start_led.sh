d22568806b87f3a7abba89c202451d3919c3a87e
Olaf Splitt <olaf.splitt@gmail.com>
2022-12-15 11:33:01 +0100

    LED Scripts

--- scripts/mupibox/mupi_start_led.sh
+++ scripts/mupibox/mupi_start_led.sh
@@ -7,3 +7,18 @@ ledPin=$(/usr/bin/jq -r .shim.ledPin ${MUPIBOX_CONFIG})
 ledMax=$(($(/usr/bin/jq -r .shim.ledBrightnessMax ${MUPIBOX_CONFIG})/100))
 
 echo "${ledPin}=${ledMax}" > /dev/pi-blaster
+
+while :
+do
+	sleep 1
+	ledPin=$(/usr/bin/jq -r .shim.ledPin ${MUPIBOX_CONFIG})
+	ledMax=$(($(/usr/bin/jq -r .shim.ledBrightnessMax ${MUPIBOX_CONFIG})/100))
+	ledMin=$(($(/usr/bin/jq -r .shim.ledBrightnessMin ${MUPIBOX_CONFIG})/100))
+	displayState=$(sudo vcgencmd display_power | grep -o '.$')
+	if [ displayState = 0 ]
+	then
+		echo "${ledPin}=${ledMin}" > /dev/pi-blaster
+	else
+		echo "${ledPin}=${ledMax}" > /dev/pi-blaster		
+	fi
+done
