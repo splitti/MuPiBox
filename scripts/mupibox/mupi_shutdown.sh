61c70b6664080a7af4a796574f089dda8c2ba90b
Olaf Splitt <olaf.splitt@gmail.com>
2022-12-15 20:41:45 +0100

    Optimizing LED Control

--- scripts/mupibox/mupi_shutdown.sh
+++ scripts/mupibox/mupi_shutdown.sh
@@ -4,6 +4,8 @@
 # 2. Update settings
 # 3. Plays shutdown sound
 
+nohup /usr/local/bin/mupibox/./mupi_stop_led.sh &
+
 CONFIG="/etc/mupibox/mupiboxconfig.json"
 SHUT_SPLASH=$(/usr/bin/jq -r .mupibox.shutSplash ${CONFIG})
 
--- scripts/mupibox/shutdown.sh
+++ scripts/mupibox/shutdown.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 #
 
-/usr/local/bin/mupibox/./mupi_stop_led.sh &
-
 sleep 1.5
 sudo poweroff
