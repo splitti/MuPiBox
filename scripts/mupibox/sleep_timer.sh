ccbb9ca607844e6abf987daa7f9f3d01d067912a
Olaf Splitt <olaf.splitt@gmail.com>
2022-12-24 23:41:54 +0100

    Update sleep_timer.sh

--- scripts/mupibox/sleep_timer.sh
+++ scripts/mupibox/sleep_timer.sh
@@ -4,11 +4,14 @@
 
 sleeptimer=$1
 currtime=0
+sec2sleep=60
+
+echo ${sleeptimer} > /tmp/.time2sleep
 
 while ${sleeptimer} >= ${currtime}:
 do
-	sleep 60
-	currtime=${currtime}+1
+	sleep ${sec2sleep}
+	currtime=${currtime}+${sec2sleep}
 	resttime=${sleeptimer}-${currtime}
 	echo ${resttime} > /tmp/.time2sleep
 done
