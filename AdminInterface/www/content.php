b4bd5adeea07b0c352a5a95be215d38a6adc161e
Olaf Splitt <olaf.splitt@gmail.com>
2023-02-09 23:07:49 +0100

    Bugfix

--- AdminInterface/www/content.php
+++ AdminInterface/www/content.php
@@ -6,7 +6,8 @@
 <p>Audio books, music and streams can be added here. Control is the same as on the display, but the display is not synchronized. Playing music cannot be started here.</p>
 <?php
         $ip=exec("hostname -I | awk '{print $1}'");
-        print "<p><embed src='http://".$data["mupibox"]["host"].":8200' id='remotecontrol' width='800px' height='480px'></p>";
+		
+        print "<div style='max-width:800px;'><p><embed src='http://".$data["mupibox"]["host"].":8200' id='remotecontrol' width='800px' height='480px'></p></div>";
         print "<p><a href='http://".$data["mupibox"]["host"].":8200' id='remotecontrol' target='_blank'>If it doesn't display properly or can't be served, try this Link and click me...</a></p>";
 
 ?>
--- AdminInterface/www/theme-data/earth/Readme.md
+++
@@ -1,3 +0,0 @@
-# Sources
-Image: https://pixabay.com/users/piro4d-2707530/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1990298
-Font: https://www.dafont.com/de/nasa21.font
