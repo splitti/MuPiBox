d3dab4b26e97895b7e9372eb744693bfaf63e42a
Olaf Splitt <olaf.splitt@gmail.com>
2023-02-12 21:16:39 +0100

    Steampunk Update

--- themes/steampunk/Readme.md
+++ themes/steampunk/Readme.md
@@ -1,5 +1,5 @@
 # Sources
-Image: 
-Gear:
-Button-Background:
-Font: https://www.dafont.com/de/shadow-of-xizor.font
+Background: https://pixabay.com/illustrations/automobile-map-of-the-world-writing-3236000/
+Gear: https://pixabay.com/illustrations/gear-steampunk-rust-mechanism-6531453/
+Header-/Button-Background: https://pixabay.com/illustrations/steampunk-gears-clock-brass-rust-5175260/
+Font: https://www.dafont.com/de/akaposse.font
--- update/start_mupibox_update-developer.sh
+++ update/start_mupibox_update-developer.sh
@@ -70,9 +70,9 @@ sudo service mupi_idle_shutdown stop
 	sudo wget ${SRC}/themes/earth/earth-bg.jpg -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/earth/earth-bg.jpg >&3 2>&3
 	sudo wget ${SRC}/themes/earth/Nasa21.ttf -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/earth/Nasa21.ttf >&3 2>&3
 	sudo wget ${SRC}/themes/steampunk/steampunk-bg.jpg -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk/steampunk-bg.jpg >&3 2>&3
-	sudo wget ${SRC}/themes/steampunk/SHAXIZOR.TTF -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk/SHAXIZOR.TTF >&3 2>&3
+	sudo wget ${SRC}/themes/steampunk/akaPosse.ttf -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk/akaPosse.ttf >&3 2>&3
 	sudo wget ${SRC}/themes/steampunk/steampunk-gear.png -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk/steampunk-gear.png >&3 2>&3
-	sudo wget ${SRC}/themes/steampunk/header.jpg -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk/header.jpg >&3 2>&3
+	sudo wget ${SRC}/themes/steampunk/steampunk-header.jpg -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk/steampunk-header.jpg >&3 2>&3
 	sudo chown dietpi:dietpi -R /home/dietpi/.mupibox/Sonos-Kids-Controller-master/
 
 	sudo wget ${SRC}/themes/earth.css -O /home/dietpi/MuPiBox/themes/earth.css >&3 2>&3
