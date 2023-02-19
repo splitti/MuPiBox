87dd263b0089c5d94a312d1420b58579a16d065d
Olaf Splitt <olaf.splitt@gmail.com>
2023-02-20 00:27:03 +0100

    Theme Update

---
+++ themes/matrix/Readme.md
@@ -0,0 +1,3 @@
+# Sources
+Background: https://pixabay.com/vectors/matrix-background-wallpaper-5361690/
+Font: https://www.dafont.com/de/pixolletta8px.font
--- update/start_mupibox_update-developer.sh
+++ update/start_mupibox_update-developer.sh
@@ -66,12 +66,16 @@ sudo service mupi_idle_shutdown stop
 	sudo mkdir -p /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/earth >&3 2>&3
 	sudo mkdir -p /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/steampunk >&3 2>&3
 	sudo mkdir -p /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/fantasybutterflies >&3 2>&3
+	sudo mkdir -p /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/matrix >&3 2>&3
 	
 	#FANTASY-BUTTERFLIES
 	sudo wget ${SRC}/themes/fantasybutterflies/odstemplikBold.otf -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/fantasybutterflies/odstemplikBold.otf >&3 2>&3
 	sudo wget ${SRC}/themes/fantasybutterflies/fantasy-butterflies-bg.jpg -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/fantasybutterflies/fantasy-butterflies-bg.jpg >&3 2>&3
 	sudo wget ${SRC}/themes/fantasybutterflies/fantasy-circle-bg.png -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/fantasybutterflies/fantasy-circle-bg.png >&3 2>&3
 
+	#MATRIX
+	sudo wget ${SRC}/themes/matrix/matrix-bg.png -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/matrix/matrix-bg.png >&3 2>&3
+	sudo wget ${SRC}/themes/matrix/Pixolletta8px.ttf -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/matrix/Pixolletta8px.ttf >&3 2>&3
 	
 	#EARTH
 	sudo wget ${SRC}/themes/earth/earth-bg.jpg -O /home/dietpi/.mupibox/Sonos-Kids-Controller-master/www/theme-data/earth/earth-bg.jpg >&3 2>&3
