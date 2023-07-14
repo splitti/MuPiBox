9b563a4d823236f74d6127e59d4e4dce7a0625fb
nero0345 <nero.erik@googlemail.com>
2023-07-14 23:42:31 +0200

    Update telegram_Track_Spotify.py

--- scripts/mupibox/telegram_Track_Spotify.py
+++ scripts/mupibox/telegram_Track_Spotify.py
@@ -13,24 +13,25 @@ with open("/etc/mupibox/mupiboxconfig.json") as file:
 url = 'http://127.0.0.1:5005/state'
 state = requests.get(url).json()
 
-urls = 'http://127.0.0.1:5005/show'
+urls = 'http://127.0.0.1:5005/episode'
 
 TOKEN = config['telegram']['token']
 bot = telepot.Bot(TOKEN)
 chat_id = config['telegram']['chatId']
 
+if state['currently_playing_type'] == 'episode':
+    episode = requests.get(urls).json()
+    msg = episode['show']['name'] + "\n" + episode['name']
+    bot.sendMessage(chat_id, msg)
+    subprocess.run(["sudo", "rm", "/tmp/telegram_screen.png"])
+    subprocess.run(["sudo", "DISPLAY=:0", "scrot", "/tmp/telegram_screen.png"])
+    bot.sendPhoto(chat_id, open('/tmp/telegram_screen.png', 'rb'))
+    sys.exit()
+
 track_old = 0
 track_new = state['item']['track_number']
 
 while state['is_playing']:
-    if stat['currently_playing_type'] = 'episode':
-        show = requests.get(urls).json()
-        msg = show['show']['name'] + "\n" + show['name']
-        bot.sendMessage(chat_id, msg)
-        subprocess.run(["sudo", "rm", "/tmp/telegram_screen.png"])
-        subprocess.run(["sudo", "DISPLAY=:0", "scrot", "/tmp/telegram_screen.png"])
-        bot.sendPhoto(chat_id, open('/tmp/telegram_screen.png', 'rb'))
-        break
     if track_old != track_new:
         msg = state['item']['album']['name'] + "\n" + state['item']['name'] + "\nTrack: " + str(state['item']['track_number']) + "/" + str(state['item']['album']['total_tracks'])
         bot.sendMessage(chat_id, msg)
