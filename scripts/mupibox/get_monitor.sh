385fa81347d5cd527418e8a9c1db580a1f62c1de
nero0345 <nero.erik@googlemail.com>
2023-08-10 00:19:14 +0200

    Update get_monitor.sh

--- scripts/mupibox/get_monitor.sh
+++ scripts/mupibox/get_monitor.sh
@@ -3,16 +3,19 @@
 # Get monitor blank information to block inputs if the screen is blank.
 
 MONITOR_FILE="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/monitor.json"
+minimumsize=18
 
 while true
 do
+
+        actualsize=$(wc -c <"${MONITOR_FILE}")
 
         if [ ! -f ${MONITOR_FILE} ]; then
                 sudo echo -n "{}" ${MONITOR_FILE}
                 sudo chown dietpi:dietpi ${MONITOR_FILE}
                 /usr/bin/cat <<< $(/usr/bin/jq -n --arg v "On" '.monitor = $v' ${MONITOR_FILE}) >  ${MONITOR_FILE}
-        elif [ -z ${MONITOR_FILE} ]; then
-                rm ${MONITOR_FILE}
+        elif [ $actualsize -le $minimumsize ]; then
+                sudo rm ${MONITOR_FILE}
                 sudo echo -n "{}" ${MONITOR_FILE}
                 sudo chown dietpi:dietpi ${MONITOR_FILE}
                 /usr/bin/cat <<< $(/usr/bin/jq -n --arg v "On" '.monitor = $v' ${MONITOR_FILE}) >  ${MONITOR_FILE}
