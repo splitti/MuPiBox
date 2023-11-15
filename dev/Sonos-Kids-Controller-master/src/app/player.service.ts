88aa7f288646ab396f91a028fc09d9e0aac36b80
nero0345 <nero.erik@googlemail.com>
2023-11-15 11:43:12 +0100

    request over ip

--- dev/Sonos-Kids-Controller-master/src/app/player.service.ts
+++ dev/Sonos-Kids-Controller-master/src/app/player.service.ts
@@ -168,7 +168,7 @@ export class PlayerService {
 
   private sendRequest(url: string) {
     this.getConfig().subscribe(config => {
-      const baseUrl = 'http://' + config.server + ':' + config.port + '/' + config.rooms[0] + '/';
+      const baseUrl = 'http://' + config.ip + ':' + config.port + '/' + config.rooms[0] + '/';
       this.http.get(baseUrl + url).subscribe();
     });
   }
