3b8575f7afda4a6493346e31e3166f53bd60e84e
Olaf Splitt <olaf.splitt@gmail.com>
2023-12-22 03:35:02 +0100

    Bugfix

--- scripts/mupibox/auto_add_wifi.sh
+++ scripts/mupibox/auto_add_wifi.sh
@@ -2,7 +2,9 @@
 
 JSON_TEMPLATE="https://raw.githubusercontent.com/splitti/MuPiBox/main/config/templates/add_wifi.json"
 WIFI_FILE="/boot/add_wifi.json"
+FIRST_INSTALL="/boot/mupi.install"
 WPACONF="/etc/wpa_supplicant/wpa_supplicant.conf"
+MUPIBOX_CONFIG="/etc/mupibox/mupiboxconfig.json"
 
 if [ -f "$WIFI_FILE" ]; then
 	SSID=$(/usr/bin/jq -r .ssid ${WIFI_FILE})
@@ -16,4 +18,12 @@ if [ -f "$WIFI_FILE" ]; then
 	fi
 else
 	sudo wget -q -O ${WIFI_FILE} ${JSON_TEMPLATE}	
+fi
+
+sleep 30
+if [ -f "$FIRST_INSTALL" ]; then
+	sudo rm ${FIRST_INSTALL}
+	VERSION=$(/usr/bin/jq -r .version ${MUPIBOX_CONFIG})
+	CPU=$(cat /proc/cpuinfo | grep Serial | cut -d ":" -f2 | sed 's/^ //')
+	curl -X POST https://mupibox.de/mupi/ct.php -H "Content-Type: application/x-www-form-urlencoded" -d key1=${CPU} -d key2=Installation -d key3="${VERSION}"
 fi
