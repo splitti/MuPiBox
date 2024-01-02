9995f1fe1c13ace7499095e1d8ff29958b799468
Ronny Schönfeld <ronny@ronbal.de>
2024-01-02 14:29:22 +0100

    Update sleep_timer.sh wled integration

--- scripts/mupibox/sleep_timer.sh
+++ scripts/mupibox/sleep_timer.sh
@@ -8,6 +8,15 @@ sec2sleep=1
 
 echo ${sleeptimer} > /tmp/.time2sleep
 
+CONFIG="/etc/mupibox/mupiboxconfig.json"
+wled_active=$(/usr/bin/jq -r .wled.active ${CONFIG})
+wled_baud_rate=$(/usr/bin/jq -r .wled.baud_rate ${CONFIG})
+wled_com_port=$(/usr/bin/jq -r .wled.com_port ${CONFIG})
+if [ "${wled_active}" = "true" ]; then
+wled_data='{"nl":{"dur":'$((sleeptimer/60))'}},{"nl":{"on":true}},{"v":true}'
+sudo python3 /usr/local/bin/mupibox/wled.py $wled_com_port $wled_baud_rate $wled_data
+fi
+
 while (( ${sleeptimer} >= ${currtime} )); do
 	sleep ${sec2sleep}
 	currtime=$((${currtime}+${sec2sleep}))
