1cf6e4f156aef6129ffdd9e292fdc321353a6cb0
Olaf Splitt <olaf.splitt@gmail.com>
2024-01-10 02:01:51 +0100

    Update mupi_start_led.sh

--- scripts/mupibox/mupi_start_led.sh
+++ scripts/mupibox/mupi_start_led.sh
@@ -7,9 +7,7 @@ TMP_LEDFILE="/tmp/.power_led"
 ledPin=$(/usr/bin/jq -r .shim.ledPin ${MUPIBOX_CONFIG})
 ledMax=$(/usr/bin/jq -r .shim.ledBrightnessMax ${MUPIBOX_CONFIG})
 ledMin=$(/usr/bin/jq -r .shim.ledBrightnessMin ${MUPIBOX_CONFIG})
-#ledMax=$(echo "scale=2; $ledMax/100" | bc)
 
-#echo "${ledPin}=${ledMax}" > /dev/pi-blaster
 echo "{}" | tee ${TMP_LEDFILE}
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${ledPin}" '.led_gpio = $v' ${TMP_LEDFILE}) >  ${TMP_LEDFILE}
 /usr/bin/cat <<< $(/usr/bin/jq --arg v "${ledMax}" '.led_max_brightness = $v' ${TMP_LEDFILE}) >  ${TMP_LEDFILE}
