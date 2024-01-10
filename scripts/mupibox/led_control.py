141db5bc661af2afc9f4bb9be39ec63ab606b9ec
Olaf Splitt <olaf.splitt@gmail.com>
2024-01-10 02:35:57 +0100

    Update led_control.py

--- scripts/mupibox/led_control.py
+++ scripts/mupibox/led_control.py
@@ -14,38 +14,40 @@ POWER_LED = PWMLED(json_data["led_gpio"])
 
 
 def sigterm_handler(*_):
-    with open(JSON_DATA_FILE) as json_file:
-        json_data = json.load(json_file)
-    for x in range(int(json_data["led_max_brightness"]), 0):
-        x -= 1
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(0, int(json_data["led_max_brightness"])):
-        x += 1
+    for x in range(0, int(json_data["led_max_brightness"]), +1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(int(json_data["led_max_brightness"]), 0):
-        x -= 1
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(0, int(json_data["led_max_brightness"])):
-        x += 1
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(int(json_data["led_max_brightness"]), 0):
-        x -= 1
+    for x in range(0, int(json_data["led_max_brightness"]), +1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(int(json_data["led_max_brightness"]), 0):
-        x -= 1
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(0, int(json_data["led_max_brightness"])):
-        x += 1
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
         POWER_LED.value = x/100
         sleep(0.01)
-    for x in range(int(json_data["led_max_brightness"]), 0):
-        x -= 1
+    for x in range(0, int(json_data["led_max_brightness"]), +1):
+        POWER_LED.value = x/100
+        sleep(0.01)
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
+        POWER_LED.value = x/100
+        sleep(0.01)
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
+        POWER_LED.value = x/100
+        sleep(0.01)
+    for x in range(0, int(json_data["led_max_brightness"]), +1):
+        POWER_LED.value = x/100
+        sleep(0.01)
+    for x in range(int(json_data["led_max_brightness"]), 0, -1):
         POWER_LED.value = x/100
         sleep(0.01)
     sys.exit(0)
@@ -64,19 +66,14 @@ def main():
     while True:
         with open(JSON_DATA_FILE) as json_file:
             json_data = json.load(json_file)
-        print(json_data["led_dim_mode"])
         if json_data["led_dim_mode"] == "0" and json_data["led_dim_mode"] != LED_DIM_MODE_LAST:
             for x in range(int(json_data["led_min_brightness"]), int(json_data["led_max_brightness"]), +1):
-                #x += 1
                 POWER_LED.value = x/100
                 sleep(0.02)
-                print("YES")
         if json_data["led_dim_mode"] == "1" and json_data["led_dim_mode"] != LED_DIM_MODE_LAST:
             for x in range(int(json_data["led_max_brightness"]), int(json_data["led_min_brightness"]), -1):
-                #x = x - 1
                 POWER_LED.value = x/100
                 sleep(0.02)
-                print("YES")
         LED_DIM_MODE_LAST = json_data["led_dim_mode"]
         sleep(1)
 
