a913daf1cd286301c507e23034242d8bea693546
nero0345 <nero.erik@googlemail.com>
2024-02-06 17:07:17 +0100

    Update albumstop.sh

--- scripts/mupibox/albumstop.sh
+++ scripts/mupibox/albumstop.sh
@@ -9,9 +9,10 @@ if [ ! -f ${ALBUMSTOP_FILE} ]; then
         sudo echo -n "{}" ${ALBUMSTOP_FILE}
         chown dietpi:dietpi ${ALBUMSTOP_FILE}
         /usr/bin/cat <<< $(/usr/bin/jq -n --arg v "Off" '.albumStop = $v' ${ALBUMSTOP_FILE}) >  ${ALBUMSTOP_FILE}
+        sleep 10
+        sudo bash /usr/local/bin/mupibox/shutdown.sh
 else
         /usr/bin/cat <<< $(/usr/bin/jq --arg v "Off" '.albumStop = $v' ${ALBUMSTOP_FILE}) >  ${ALBUMSTOP_FILE}
+        sleep 10
+        sudo bash /usr/local/bin/mupibox/shutdown.sh
 fi
-
-sleep 2
-sudo poweroff
