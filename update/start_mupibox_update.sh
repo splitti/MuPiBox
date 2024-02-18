b92b89766b441c93ddc7b466eda1441c78699970
Olaf Splitt <olaf.splitt@gmail.com>
2024-02-18 11:27:10 +0100

    Update start_mupibox_update.sh

--- update/start_mupibox_update.sh
+++ update/start_mupibox_update.sh
@@ -425,10 +425,9 @@ echo "==========================================================================
 	chown -R dietpi:www-data /home/dietpi/MuPiBox/media/cover >&3 2>&3
 
 	echo -e "XXX\n${STEP}\nUpdate Config-File... \nXXX"	
-	cd ${MUPI_SRC}/update/	>&3 2>&3
-	cp ${MUPI_SRC}/update/json/*.json /tmp/ >&3 2>&3
-	chmod 755 conf_update.sh >&3 2>&3
-	./conf_update.sh >&3 2>&3
+	usr/bin/cat <<< $(/usr/bin/jq -s '.[0] * .[1]' ${CONFIG} ${MUPI_SRC}/config/templates/mupiboxconfig.json) >  ${CONFIG}
+	#chmod 755 conf_update.sh >&3 2>&3
+	#./conf_update.sh >&3 2>&3
 	
 	###############################################################################################
 
