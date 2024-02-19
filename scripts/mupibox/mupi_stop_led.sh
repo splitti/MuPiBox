c0c4882636a5bde9861f8c8b43896d2e5f514912
splitti <45425109+splitti@users.noreply.github.com>
2024-02-19 02:02:34 +0100

    Update mupi_stop_led.sh

--- scripts/mupibox/mupi_stop_led.sh
+++ scripts/mupibox/mupi_stop_led.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
 
-ps -ef | grep 'led_control.py' | grep -v grep | awk '{print $2}' | xargs kill > /dev/null
+ps -ef | grep 'led_control' | grep -v grep | awk '{print $2}' | xargs kill > /dev/null
 ps -ef | grep 'mupi_start_led.sh' | grep -v grep | awk '{print $2}' | xargs kill > /dev/null
