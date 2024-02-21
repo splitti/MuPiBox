03be4b6afa4f31e7b22ffb214a3b1c90430e8da4
nero0345 <nero.erik@googlemail.com>
2024-02-21 03:57:32 +0100

    test

--- dev/Sonos-Kids-Controller-master/src/app/media.service.ts
+++ dev/Sonos-Kids-Controller-master/src/app/media.service.ts
@@ -388,7 +388,7 @@ export class MediaService {
     return this.artistMediaSubject.pipe(
       map((media: Media[]) => {
         return media
-          .filter(currentMedia => (currentMedia.artist === artist.name && currentMedia.category === "resume"))
+          .filter(currentMedia => currentMedia.category === "resume")
           .sort((a, b) => a.title.localeCompare(b.title, undefined, {
             numeric: true,
             sensitivity: 'base'
