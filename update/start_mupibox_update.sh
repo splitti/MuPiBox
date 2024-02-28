020fb4bdbd4e733673b8f4653b91bb96003dfffd
Olaf Splitt <olaf.splitt@gmail.com>
2024-02-28 16:53:50 +0100

    Update start_mupibox_update.sh

--- update/start_mupibox_update.sh
+++ update/start_mupibox_update.sh
@@ -31,12 +31,12 @@ VERSION=$(/usr/bin/jq -r .release.${RELEASE}[-1].version ${VER_JSON})  >&3 2>&3
 MUPIBOX_URL=$(/usr/bin/jq -r .release.${RELEASE}[-1].url ${VER_JSON})  >&3 2>&3
 USER=$(/usr/bin/whoami) >&3 2>&3
 RASPPI=$(/usr/bin/cat /sys/firmware/devicetree/base/model | tr -d '\0' ) >&3 2>&3
-if [ ${RELEASE} = "dev" ]; then
+if [ "${RELEASE}" = "dev" ]; then
 	MUPI_SRC="/home/dietpi/MuPiBox-main" >&3 2>&3
 else
 	MUPI_SRC="/home/dietpi/MuPiBox-${VERSION}" >&3 2>&3
 fi
-if [[ "$RELEASE"!="dev" ]]; then
+if [ "$RELEASE"!="dev" ]; then
 	VERSION_LONG="${VERSION} ${RELEASE}"
 else
 	VERSION_LONG="DEV $(curl -s "https://api.github.com/repos/splitti/MuPiBox" | jq -r '.pushed_at' | cut -d'T' -f1)"  >&3 2>&3
