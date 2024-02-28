10a2e07c1dfa4068d2d91d73392da1312458efda
Olaf Splitt <olaf.splitt@gmail.com>
2024-02-28 13:11:16 +0100

    Update start_mupibox_update.sh

--- update/start_mupibox_update.sh
+++ update/start_mupibox_update.sh
@@ -36,7 +36,7 @@ if [ ${RELEASE} = "dev" ]; then
 else
 	MUPI_SRC="/home/dietpi/MuPiBox-${VERSION}" >&3 2>&3
 fi
-if [ RELEASE="dev" ]; then
+if [ RELEASE=="dev" ]; then
 	VERSION_LONG="DEV $(curl -s "https://api.github.com/repos/splitti/MuPiBox" | jq -r '.pushed_at' | cut -d'T' -f1)"  >&3 2>&3
 else
 	VERSION_LONG="${VERSION} ${RELEASE}"
