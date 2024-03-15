089a97e50558e4b33e14a30feadb3aac07eb9cbd
friebi <friebi@gmx.de>
2024-03-15 11:01:58 +0100

    optimize get_monitor

--- scripts/mupibox/get_monitor.sh
+++ scripts/mupibox/get_monitor.sh
@@ -5,27 +5,25 @@
 MONITOR_FILE="/home/dietpi/.mupibox/Sonos-Kids-Controller-master/server/config/monitor.json"
 minimumsize=18
 
-while true
-do
+while true; do
+  actualsize=$(wc -c <"${MONITOR_FILE}")
 
-        actualsize=$(wc -c <"${MONITOR_FILE}")
+  if [ ! -f ${MONITOR_FILE} ]; then
+    sudo echo -n "{}" ${MONITOR_FILE}
+    sudo chown dietpi:dietpi ${MONITOR_FILE}
+    /usr/bin/cat <<<$(/usr/bin/jq -n --arg v "On" '.monitor = $v' ${MONITOR_FILE}) >${MONITOR_FILE}
+  elif [ $actualsize -le $minimumsize ]; then
+    sudo rm ${MONITOR_FILE}
+    sudo echo -n "{}" ${MONITOR_FILE}
+    sudo chown dietpi:dietpi ${MONITOR_FILE}
+    /usr/bin/cat <<<$(/usr/bin/jq -n --arg v "On" '.monitor = $v' ${MONITOR_FILE}) >${MONITOR_FILE}
+  else
+    MONITOR=$(sudo -H -u dietpi bash -c "DISPLAY=:0 xset q | grep 'Monitor'")
+    MONITOR=$(echo ${MONITOR} | awk '{print $3}')
+    if [ ${MONITOR} != "" ]; then
+      /usr/bin/cat <<<$(/usr/bin/jq --arg v "${MONITOR}" '.monitor = $v' ${MONITOR_FILE}) >${MONITOR_FILE}
+    fi
+  fi
 
-        if [ ! -f ${MONITOR_FILE} ]; then
-                sudo echo -n "{}" ${MONITOR_FILE}
-                sudo chown dietpi:dietpi ${MONITOR_FILE}
-                /usr/bin/cat <<< $(/usr/bin/jq -n --arg v "On" '.monitor = $v' ${MONITOR_FILE}) >  ${MONITOR_FILE}
-        elif [ $actualsize -le $minimumsize ]; then
-                sudo rm ${MONITOR_FILE}
-                sudo echo -n "{}" ${MONITOR_FILE}
-                sudo chown dietpi:dietpi ${MONITOR_FILE}
-                /usr/bin/cat <<< $(/usr/bin/jq -n --arg v "On" '.monitor = $v' ${MONITOR_FILE}) >  ${MONITOR_FILE}
-        else
-                MONITOR=$(sudo -H -u dietpi bash -c "DISPLAY=:0 xset q | grep 'Monitor'")
-				MONITOR=$(echo ${MONITOR} | awk '{print $3}')
-				if [ ${MONITOR} != "" ]; then
-					/usr/bin/cat <<< $(/usr/bin/jq --arg v "${MONITOR}" '.monitor = $v' ${MONITOR_FILE}) >  ${MONITOR_FILE}
-				fi
-        fi
-
-	sleep 1
+  sleep 30
 done
