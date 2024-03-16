e17f99ab5327aba794d4757fdf64c509a61c45ab
friebi <friebi@gmx.de>
2024-03-16 21:59:36 +0100

    make off_trigger Kernel 6.6 compatible added dependency to gpiod apt package
    # Conflicts:
    #	scripts/OnOffShim/off_trigger.sh

--- scripts/OnOffShim/off_trigger.sh
+++ scripts/OnOffShim/off_trigger.sh
@@ -12,34 +12,27 @@ sudo chmod 777 /tmp/.rrd/*.rrd
 
 sleep 10
 
-CONFIG="/etc/mupibox/mupiboxconfig.json"
-TRIGGER_PIN=$(/usr/bin/jq -r .shim.triggerPin ${CONFIG})
-PRESS_DELAY=$(/usr/bin/jq -r .timeout.pressDelay ${CONFIG})
-START_VOLUME=$(/usr/bin/jq -r .mupibox.startVolume ${CONFIG})
+MUPIBOX_CONFIG="/etc/mupibox/mupiboxconfig.json"
+TRIGGER_PIN=$(/usr/bin/jq -r .shim.triggerPin ${MUPIBOX_CONFIG})
+PRESS_DELAY=$(/usr/bin/jq -r .timeout.pressDelay ${MUPIBOX_CONFIG})
+START_VOLUME=$(/usr/bin/jq -r .mupibox.startVolume ${MUPIBOX_CONFIG})
 
 # Check if OnOff-Button is pressed
-/bin/echo ${TRIGGER_PIN} > /sys/class/gpio/export
-/bin/echo in > /sys/class/gpio/gpio${TRIGGER_PIN}/direction
-
-power=$(cat /sys/class/gpio/gpio${TRIGGER_PIN}/value)
+power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
 [ $power = 0 ] && switchtype="1" #Not a momentary button
 [ $power = 1 ] && switchtype="0" #Momentary button
 
 until [ $power = $switchtype ]; do
-    power=$(cat /sys/class/gpio/gpio${TRIGGER_PIN}/value)
-	if [ $power = $switchtype ]; then
-		sleep ${PRESS_DELAY}
-		power=$(cat /sys/class/gpio/gpio${TRIGGER_PIN}/value)
-		/usr/bin/pactl set-sink-volume @DEFAULT_SINK@ ${START_VOLUME}% 
-		/usr/bin/aplay /home/dietpi/MuPiBox/sysmedia/sound/button_shutdown.wav
-		#/usr/bin/mplayer -volume ${START_VOLUME} ${START_SOUND} &
-	fi
-    sleep 0.05
+  power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
+  if [ $power = $switchtype ]; then
+    sleep ${PRESS_DELAY}
+    power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
+    /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ ${START_VOLUME}%
+  fi
+  sleep 1
 done
 
 sudo service mupi_startstop stop
-#sudo su - -c 'nohup /usr/local/bin/mupibox/./mupi_stop_led.sh > /dev/null 2>&1 &'
-sudo service mupi_powerled stop 
+sudo service mupi_powerled stop
 
-#sudo shutdown -h now
 poweroff
