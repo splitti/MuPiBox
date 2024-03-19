4132b8de622ffacd86c7d85a8a6a77c5cfd07933
friebi <friebi@gmx.de>
2024-03-19 10:43:30 +0100

    Make poweroff.sh Kernel 6.6 compatible
    # Conflicts:
    #	scripts/OnOffShim/off_trigger.sh

--- scripts/OnOffShim/off_trigger.sh
+++ scripts/OnOffShim/off_trigger.sh
@@ -5,9 +5,9 @@
 # See this as a working example
 
 sudo mkdir /tmp/.rrd
-sudo rrdtool create /tmp/.rrd/cputemp.rrd  --start now  --step 10  --no-overwrite  DS:cpu_temp:GAUGE:120:U:U  RRA:AVERAGE:0.5:1:120
-sudo rrdtool create /tmp/.rrd/ram.rrd  --start now  --step 10  --no-overwrite  DS:ram:GAUGE:120:U:U  RRA:AVERAGE:0.5:1:120  DS:swap:GAUGE:120:U:U  RRA:AVERAGE:0.5:1:120
-sudo rrdtool create /tmp/.rrd/cpuusage.rrd --start now  --step 10  --no-overwrite  DS:load1:GAUGE:120:0:U  DS:load5:GAUGE:120:0:U  DS:load15:GAUGE:120:0:U  RRA:AVERAGE:0.5:1:120  RRA:AVERAGE:0.5:5:120  RRA:AVERAGE:0.5:15:120  RRA:AVERAGE:0.5:60:120
+sudo rrdtool create /tmp/.rrd/cputemp.rrd --start now --step 10 --no-overwrite DS:cpu_temp:GAUGE:120:U:U RRA:AVERAGE:0.5:1:120
+sudo rrdtool create /tmp/.rrd/ram.rrd --start now --step 10 --no-overwrite DS:ram:GAUGE:120:U:U RRA:AVERAGE:0.5:1:120 DS:swap:GAUGE:120:U:U RRA:AVERAGE:0.5:1:120
+sudo rrdtool create /tmp/.rrd/cpuusage.rrd --start now --step 10 --no-overwrite DS:load1:GAUGE:120:0:U DS:load5:GAUGE:120:0:U DS:load15:GAUGE:120:0:U RRA:AVERAGE:0.5:1:120 RRA:AVERAGE:0.5:5:120 RRA:AVERAGE:0.5:15:120 RRA:AVERAGE:0.5:60:120
 sudo chmod 777 /tmp/.rrd/*.rrd
 
 sleep 10
@@ -22,15 +22,15 @@ power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
 [ $power = 0 ] && switchtype="1" #Not a momentary button
 [ $power = 1 ] && switchtype="0" #Momentary button
 
-until [ $power = $switchtype ]; do
-  power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
-  if [ $power = $switchtype ]; then
-    sleep ${PRESS_DELAY}
-    power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
-    /usr/bin/pactl set-sink-volume @DEFAULT_SINK@ ${START_VOLUME}%
+until [ ${power} == ${switchtype} ]; do
+  if [ ${power} == ${switchtype} ]; then
+    sleep $((${PRESS_DELAY} - 1))
   fi
   sleep 1
+  power=$(/usr/bin/gpioget $(/usr/bin/gpiofind GPIO${TRIGGER_PIN}))
 done
+
+/usr/bin/pactl set-sink-volume @DEFAULT_SINK@ ${START_VOLUME}%
 
 sudo service mupi_startstop stop
 sudo service mupi_powerled stop
--- scripts/OnOffShim/poweroff.sh
+++ scripts/OnOffShim/poweroff.sh
@@ -4,16 +4,11 @@
 # This is mandatory for proper SHIM shutdown!
 
 CONFIG="/etc/mupibox/mupiboxconfig.json"
-POWEROFF_PIN=$(/usr/bin/jq -r .shim.poweroffPin ${CONFIG})
-CUT_PIN=$(/usr/bin/jq -r .shim.cutPin ${CONFIG})
+POWEROFF_PIN=$(/usr/bin/jq -r .shim.poweroffPin $CONFIG)
+CUT_PIN=$(/usr/bin/jq -r .shim.cutPin $CONFIG)
 
-if [ "$1" = "poweroff" ]; then
-    # added by schlizbäda:
-    /bin/echo ${CUT_PIN} > /sys/class/gpio/export
-    /bin/echo out > /sys/class/gpio/gpio${CUT_PIN}/direction
-    /bin/echo 1 > /sys/class/gpio/gpio${CUT_PIN}/value
-
-    /bin/echo ${POWEROFF_PIN} > /sys/class/gpio/export
-    /bin/echo out > /sys/class/gpio/gpio${POWEROFF_PIN}/direction
-    /bin/echo 0 > /sys/class/gpio/gpio${POWEROFF_PIN}/value
+if [ "$1" == "poweroff" ]; then
+    # added by schlizbaeda:
+    /usr/bin/gpioset $(/usr/bin/gpiofind GPIO$TRIGGER_PIN)=1
+    /usr/bin/gpioset $(/usr/bin/gpiofind GPIO$POWEROFF_PIN)=0
 fi
