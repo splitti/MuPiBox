b7b571cc00d696013a5f8838daf3a12e9d40112e
friebi <friebi@gmx.de>
2024-04-15 21:44:46 +0200

    fix typo

--- scripts/OnOffShim/poweroff.sh
+++ scripts/OnOffShim/poweroff.sh
@@ -3,12 +3,12 @@
 # OnOff SHIM exposed by cyperghost for retropie.org.uk
 # This is mandatory for proper SHIM shutdown!
 
-CONFIG="/etc/mupibox/mupiboxconfig.json"
-POWEROFF_PIN=$(/usr/bin/jq -r .shim.poweroffPin ${CONFIG})
-CUT_PIN=$(/usr/bin/jq -r .shim.cutPin ${CONFIG})
+MUPIBOX_CONFIG="/etc/mupibox/mupiboxconfig.json"
+POWEROFF_PIN=$(/usr/bin/jq -r .shim.poweroffPin ${MUPIBOX_CONFIG})
+CUT_PIN=$(/usr/bin/jq -r .shim.cutPin ${MUPIBOX_CONFIG})
 
 if [ "$1" = "poweroff" ]; then
     # added by schlizbaeda:
-    /usr/bin/gpioset $(/usr/bin/gpiofind GPIO${TRIGGER_PIN})=1
+    /usr/bin/gpioset $(/usr/bin/gpiofind GPIO${CUT_PIN})=1
     /usr/bin/gpioset $(/usr/bin/gpiofind GPIO${POWEROFF_PIN})=0
 fi
