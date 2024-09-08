39f2b3883484cca342e05af1e6ffd0a1791b1f13
Christoph <christoph1009@gmail.com>
2024-09-08 20:08:54 +0000

    started to add reverse sort toggle, started to cleanup medialist.page.ts, started to write frontend tests for slicing of media

--- dev/Sonos-Kids-Controller-master/src/app/medialist/medialist.page.spec.ts
+++ dev/Sonos-Kids-Controller-master/src/app/medialist/medialist.page.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
+import { createArtist, createMedia } from 'src/app/fixtures'
 
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
 import { MedialistPage } from './medialist.page';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing"
 
 describe('MedialistPage', () => {
-  let component: MedialistPage;
-  let fixture: ComponentFixture<MedialistPage>;
+  let component: MedialistPage
+  let fixture: ComponentFixture<MedialistPage>
   let httpClient: HttpTestingController
 
   beforeEach(() => {
@@ -23,20 +24,32 @@ describe('MedialistPage', () => {
     component = fixture.componentInstance;
     // TODO: Artist may be undefined when strictly following TS types, this should be corrected in
     // medialist.page.ts
-    component.artist = {
-      name: "Baw Batrol",
-      albumCount: "1",
-      cover: "",
-      coverMedia: {
-        type: "",
-        category: ""
-      }
-    }
+    component.artist = createArtist()
     fixture.detectChanges();
-  });
+  })
 
   it('should create', () => {
     httpClient.expectOne("http://localhost:8200/api/sonos")
     expect(component).toBeTruthy();
-  });
-});
+  })
+
+  describe('should correctly slice show media', () => {
+    it('should not slice at all', () => {
+      component.artist = createArtist({coverMedia: createMedia({ })})
+    })
+  })
+
+  it('should correctly slice artist media', () => {
+    it('should not slice at all', () => {
+      component.artist = {
+        name: "Baw Batrol",
+        albumCount: "1",
+        cover: "",
+        coverMedia: {
+          type: "",
+          category: ""
+        }
+      }
+    })
+  })
+})
--- dev/Sonos-Kids-Controller-master/src/app/medialist/medialist.page.ts
+++ dev/Sonos-Kids-Controller-master/src/app/medialist/medialist.page.ts
@@ -1,15 +1,16 @@
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { IonSlides } from '@ionic/angular';
-import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
-import { MediaService } from '../media.service';
-import { ArtworkService } from '../artwork.service';
-import { PlayerService } from '../player.service';
-import { ActivityIndicatorService } from '../activity-indicator.service';
-import { Media } from '../media';
-import { Artist } from '../artist';
-import { Monitor } from '../monitor';
 import { Observable, Subscription } from 'rxjs';
+
+import { ActivityIndicatorService } from '../activity-indicator.service';
+import { Artist } from '../artist';
+import { ArtworkService } from '../artwork.service';
+import { IonSlides } from '@ionic/angular';
+import { Media } from '../media';
+import { MediaService } from '../media.service';
+import { Monitor } from '../monitor';
 import { Mupihat } from '../mupihat';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-medialist',
@@ -70,116 +71,11 @@ export class MedialistPage implements OnInit {
   }
 
   ngOnInit() {
-    // Subscribe
     this.playerService.getConfig().subscribe(config => {
       this.hat_active = config.hat_active;
     });
 
-    console.log("this.artist", this.artist);
-    if(this.resume){
-      this.getMediaFromResumeSubscription = this.mediaService.getMediaFromResume().subscribe(media => {
-        this.media = media;
-  
-        this.media.forEach(currentMedia => {
-          this.artworkService.getArtwork(currentMedia).subscribe(url => {
-            this.covers[currentMedia.title] = url;
-          });
-        });
-
-        this.slider.update();
-
-        console.log("getMediaFromResume", this.media);
-  
-        // Workaround as the scrollbar handle isn't visible after the immediate update
-        // Seems like a size calculation issue, as resizing the browser window helps
-        // Better fix for this? 
-        window.setTimeout(() => {
-          this.slider.update();
-        }, 1000);
-      });
-    }else{
-      if((this.artist.coverMedia.showid && this.artist.coverMedia.showid.length > 0) || (this.artist.coverMedia.type == 'rss' && this.artist.coverMedia.id.length > 0)){
-        this.getMediaFromShowSubscription = this.mediaService.getMediaFromShow(this.artist).subscribe(media => {
-          this.media = media;
-    
-          this.media.forEach(currentMedia => {
-            this.artworkService.getArtwork(currentMedia).subscribe(url => {
-              this.covers[currentMedia.title] = url;
-            });
-          });
-    
-          if(this.artist.coverMedia?.aPartOfAll){
-            for (let i = 0; i < this.media.length; i++){
-              let rev = this.media.length - i;
-              if(rev >= (this.artist.coverMedia?.aPartOfAllMin) && rev <= (this.artist.coverMedia?.aPartOfAllMax)){
-                this.aPartOfAllMedia.push(this.media[i]);
-              }
-            }
-            this.media = this.aPartOfAllMedia;
-          }
-  
-          this.slider.update();
-  
-          console.log("getMediaFromShow", this.media);
-    
-          // Workaround as the scrollbar handle isn't visible after the immediate update
-          // Seems like a size calculation issue, as resizing the browser window helps
-          // Better fix for this? 
-          window.setTimeout(() => {
-            this.slider.update();
-          }, 1000);
-        });
-      } else {
-        this.getMediaFromArtistSubscription = this.mediaService.getMediaFromArtist(this.artist).subscribe(media => {
-          this.media = media;
-    
-          this.media.forEach(currentMedia => {
-            this.artworkService.getArtwork(currentMedia).subscribe(url => {
-              this.covers[currentMedia.title] = url;
-            });
-          });
-  
-          if(this.artist.coverMedia?.aPartOfAll){
-            let min: number;
-            let max: number;
-            if(this.artist.coverMedia?.aPartOfAllMin == null){
-              min = 0
-            }else{
-              min = this.artist.coverMedia?.aPartOfAllMin -1;
-            }
-            if(this.artist.coverMedia?.aPartOfAllMax == null){
-              max = parseInt(this.artist.albumCount) -1;
-            }else{
-              max = this.artist.coverMedia?.aPartOfAllMax -1;
-            }
-            console.log("Min: " + min);
-            console.log("Max: " + max);
-            console.log("media.length: " + this.media.length);
-            for (let i = 0; i < this.media.length; i++){
-              if(i >= min && i <= max){
-                this.aPartOfAllMedia.push(this.media[i]);
-              }
-            }
-            this.media = this.aPartOfAllMedia;
-          }
-  
-          this.slider.update();
-  
-          console.log("getMediaFromArtist", this.media);
-    
-          // Workaround as the scrollbar handle isn't visible after the immediate update
-          // Seems like a size calculation issue, as resizing the browser window helps
-          // Better fix for this? 
-          window.setTimeout(() => {
-            this.slider.update();
-          }, 1000);
-        });
-      }
-      //this.getMediaFromResumeSubscription = this.mediaService.getMediaFromResume().subscribe(media => {
-        //this.resumemedia = media;
-        //console.log("getMediaFromResume this.resumemedia", this.resumemedia);
-      //});
-    }
+    this.fetchMedia()
 
     // Retreive data through subscription above
     this.mediaService.publishArtistMedia();
@@ -198,8 +94,7 @@ export class MedialistPage implements OnInit {
   }
 
   ngOnDestroy(){
-    console.log("ngOnDestroy");
-     if(this.getMediaFromResumeSubscription){
+    if(this.getMediaFromResumeSubscription){
        this.getMediaFromResumeSubscription.unsubscribe();
      }
     if (this.getMediaFromShowSubscription){
@@ -210,11 +105,6 @@ export class MedialistPage implements OnInit {
     }
   }
 
-  //ionViewWillEnter() {
-    //console.log("ionViewWillEnter");
-    //this.mediaService.publishResume();
-  //}
-
   ionViewDidLeave() {
     if (this.activityIndicatorVisible) {
       this.activityIndicatorService.dismiss();
@@ -224,25 +114,19 @@ export class MedialistPage implements OnInit {
   }
 
   coverClicked(clickedMedia: Media) {
-    if(this.monitor?.monitor == "On"){
+    if (this.monitor?.monitor == "On"){
       this.activityIndicatorService.create().then(indicator => {
         this.activityIndicatorVisible = true;
         clickedMedia.index = -1;
-        console.log("search:", clickedMedia);
-        console.log("length:", this.resumemedia);
         for (let i = 0; i < this.resumemedia.length; i++) {
-          console.log("this.resumemedia[" + i + "]:", this.resumemedia[i]);
           if ((this.resumemedia[i].id && this.resumemedia[i].id === clickedMedia.id) || (this.resumemedia[i].playlistid && this.resumemedia[i].playlistid === clickedMedia.id)) {
             clickedMedia.index = i;
-            console.log("Matched by id or playlistid at index:", i);
             break;
           } else if (this.resumemedia[i].artist === clickedMedia.artist && this.resumemedia[i].id === clickedMedia.id && clickedMedia.type === 'library') {
             clickedMedia.index = i;
-            console.log("Matched by artist, id, and type 'library' at index:", i);
             break;
           }
         }
-        console.log("index at:", clickedMedia.index);
         indicator.present().then(() => {
           const navigationExtras: NavigationExtras = {
             state: {
@@ -282,5 +166,119 @@ export class MedialistPage implements OnInit {
 
   slideNext() {
     this.slider.slideNext();
+  }
+
+  private fetchMedia(): void {
+    // Method for direct and delayed slider update.
+    const updateSlider = (): void => {
+      this.slider.update();
+  
+      // Workaround as the scrollbar handle isn't visible after the immediate update
+      // Seems like a size calculation issue, as resizing the browser window helps
+      // Better fix for this? 
+      window.setTimeout(() => {
+        this.slider.update();
+      }, 1000);
+    }
+
+    // Method to fetch artwork for all media.
+    const fetchArtwork = (media: Media[]): void => {
+      media.forEach(currentMedia => {
+        this.artworkService.getArtwork(currentMedia).subscribe(url => {
+          this.covers[currentMedia.title] = url;
+        });
+      });
+    }
+
+    if (this.resume) {
+      this.getMediaFromResumeSubscription = this.mediaService.getMediaFromResume().subscribe(media => {
+        this.media = media;
+        fetchArtwork(this.media)
+        updateSlider()
+      });
+    } else {
+      const sliceMedia = (media: Media[]): Media[] => {
+        if (this.artist.coverMedia?.aPartOfAll) {
+          return media.slice(this.artist.coverMedia?.aPartOfAllMin, )
+
+          for (let i = 0; i < this.media.length; i++){
+            let rev = this.media.length - i;
+            if(rev >= (this.artist.coverMedia?.aPartOfAllMin) && rev <= (this.artist.coverMedia?.aPartOfAllMax)){
+              this.aPartOfAllMedia.push(this.media[i]);
+            }
+          }
+          this.media = this.aPartOfAllMedia;
+        }
+        return media
+
+        if(this.artist.coverMedia?.aPartOfAll){
+          let min: number;
+          let max: number;
+          if(this.artist.coverMedia?.aPartOfAllMin == null){
+            min = 0
+          }else{
+            min = this.artist.coverMedia?.aPartOfAllMin -1;
+          }
+          if(this.artist.coverMedia?.aPartOfAllMax == null){
+            max = parseInt(this.artist.albumCount) -1;
+          }else{
+            max = this.artist.coverMedia?.aPartOfAllMax -1;
+          }
+          for (let i = 0; i < this.media.length; i++){
+            if(i >= min && i <= max){
+              this.aPartOfAllMedia.push(this.media[i]);
+            }
+          }
+          this.media = this.aPartOfAllMedia;
+        }
+      }
+
+      if ((this.artist.coverMedia.showid && this.artist.coverMedia.showid.length > 0) || (this.artist.coverMedia.type == 'rss' && this.artist.coverMedia.id.length > 0)) {
+        this.getMediaFromShowSubscription = this.mediaService.getMediaFromShow(this.artist).subscribe(media => {
+          this.media = media;
+          fetchArtwork(this.media)
+    
+          if (this.artist.coverMedia?.aPartOfAll){
+            for (let i = 0; i < this.media.length; i++){
+              let rev = this.media.length - i;
+              if(rev >= (this.artist.coverMedia?.aPartOfAllMin) && rev <= (this.artist.coverMedia?.aPartOfAllMax)){
+                this.aPartOfAllMedia.push(this.media[i]);
+              }
+            }
+            this.media = this.aPartOfAllMedia;
+          }
+  
+          updateSlider()
+        });
+      } else {
+        this.getMediaFromArtistSubscription = this.mediaService.getMediaFromArtist(this.artist).subscribe(media => {
+          this.media = media;
+          fetchArtwork(this.media)
+
+          if(this.artist.coverMedia?.aPartOfAll){
+            let min: number;
+            let max: number;
+            if(this.artist.coverMedia?.aPartOfAllMin == null){
+              min = 0
+            }else{
+              min = this.artist.coverMedia?.aPartOfAllMin -1;
+            }
+            if(this.artist.coverMedia?.aPartOfAllMax == null){
+              max = parseInt(this.artist.albumCount) -1;
+            }else{
+              max = this.artist.coverMedia?.aPartOfAllMax -1;
+            }
+            for (let i = 0; i < this.media.length; i++){
+              if(i >= min && i <= max){
+                this.aPartOfAllMedia.push(this.media[i]);
+              }
+            }
+            this.media = this.aPartOfAllMedia;
+          }
+  
+          updateSlider()
+        });
+      }
+    }
   }
 }
