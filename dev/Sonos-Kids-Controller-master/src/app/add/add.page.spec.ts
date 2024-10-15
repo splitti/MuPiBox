92acdfe960f1f2ede221ad2c1b2dbc582d0de8b7
Christoph <christoph1009@gmail.com>
2024-10-15 09:43:53 +0000

    fix linting

--- dev/Sonos-Kids-Controller-master/src/app/add/add.page.spec.ts
+++ dev/Sonos-Kids-Controller-master/src/app/add/add.page.spec.ts
@@ -1,11 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing'
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing'
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing'
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { AddPage } from './add.page'
 import { FormsModule } from '@angular/forms'
-import { IonicModule } from '@ionic/angular'
 import { RouterTestingModule } from '@angular/router/testing'
+import { IonicModule } from '@ionic/angular'
+import { AddPage } from './add.page'
 
 describe('AddPage', () => {
   let component: AddPage
