6df5a27f26504bd9cf547680a833c70d48157a21
nero0345 <nero.erik@googlemail.com>
2024-11-09 01:49:35 +0100

    test

--- src/backend-player/src/spotify-control.js
+++ src/backend-player/src/spotify-control.js
@@ -619,6 +619,7 @@ function playMe(/*activePlaylist*/) {
       .play({ context_uri: activePlaylistId, offset: { position: resumeOffset }, position_ms: resumeProgess })
       .then(
         (data) => {
+          log.debug(`${nowDate.toLocaleString()}: [Spotify Debugging] Debugging started`)
           log.debug(`${nowDate.toLocaleString()}: debugging: ${muPiBoxConfig.telegram}`)
           log.debug(`${nowDate.toLocaleString()}: debugging: ${network.onlinestate}`)
           log.debug(`${nowDate.toLocaleString()}: [Spotify Control] Playback started`)
