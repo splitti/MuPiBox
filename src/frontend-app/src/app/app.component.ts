c8a8ebe227bb25a3d38d24927c11499e6fa94916
Christoph <christoph1009@gmail.com>
2024-11-12 13:10:24 +0000

    add ionic, switch to zonless (not working yet)

--- src/frontend-app/src/app/app.component.ts
+++ src/frontend-app/src/app/app.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [IonApp, IonRouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
--- src/frontend-app/src/main.ts
+++ src/frontend-app/src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone'
+import { RouteReuseStrategy, provideRouter } from '@angular/router'
+import { enableProdMode, provideExperimentalZonelessChangeDetection } from '@angular/core'
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component'
+import { bootstrapApplication } from '@angular/platform-browser'
+import { environment } from './environments/environment'
+import { routes } from './app/app.routes'
+
+if (environment.production) {
+  enableProdMode()
+}
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideExperimentalZonelessChangeDetection(),
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular({ mode: 'md' }),
+    provideRouter(routes),
+  ],
+}).catch((err) => console.error(err))
