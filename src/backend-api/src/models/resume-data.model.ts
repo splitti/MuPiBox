883860e3a628a792735be30c29edaad6e2ae3615
Christoph <christoph1009@gmail.com>
2024-11-21 10:46:40 +0000

    started work on resume data

--- src/backend-api/src/models/resume-data.model.ts
+++ src/backend-api/src/models/resume-data.model.ts
@@ -1,23 +1,12 @@
-import { Data, SpotifyData } from './data.model'
-
 import { CategoryType } from './folder.model'
+import { Data } from './data.model'
 
-export interface ResumeSpotifyData extends SpotifyData {
-  resumespotifytrack_number?: number
-  resumespotifyprogress_ms?: number
-  resumespotifyduration_ms?: number
-  resumelocalalbum?: CategoryType
-  resumelocalcurrentTracknr?: number
-  resumelocalprogressTime?: number
-  resumerssprogressTime?: number
-}
-
-export interface ResumeSpotifyData extends SpotifyData {
-  resumespotifytrack_number?: number
-  resumespotifyprogress_ms?: number
-  resumespotifyduration_ms?: number
-  resumelocalalbum?: CategoryType
-  resumelocalcurrentTracknr?: number
-  resumelocalprogressTime?: number
-  resumerssprogressTime?: number
-}
+// export interface ResumeData extends Data {
+//   resumespotifytrack_number?: number
+//   resumespotifyprogress_ms?: number
+//   resumespotifyduration_ms?: number
+//   resumelocalalbum?: CategoryType
+//   resumelocalcurrentTracknr?: number
+//   resumelocalprogressTime?: number
+//   resumerssprogressTime?: number
+// }
