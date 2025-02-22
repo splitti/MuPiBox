27d942c2379b2b4010df31e1e1179f6a074f9246
Christoph <christoph1009@gmail.com>
2025-02-22 10:06:08 +0000

    further work on add/edit page form

--- src/frontend-app/src/app/add-edit/add-edit.page.ts
+++ src/frontend-app/src/app/add-edit/add-edit.page.ts
@@ -1,3 +1,4 @@
+import { CategoryType, Sorting } from '@backend-api/folder.model'
 import { ChangeDetectionStrategy, Component, computed, model, signal } from '@angular/core'
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
 import {
@@ -30,7 +31,6 @@ import {
   IonToolbar,
 } from '@ionic/angular/standalone'
 
-import { CategoryType } from '@backend-api/folder.model'
 import { SourceType } from '@backend-api/data.model'
 import { toSignal } from '@angular/core/rxjs-interop'
 
@@ -71,7 +71,17 @@ export enum AddEditPageSourceType {
 export class AddEditPage {
   protected sourceTypeControl = new FormControl<AddEditPageSourceType | undefined>(undefined, Validators.required)
   protected sourceUrl = new FormControl<string | undefined>(undefined, Validators.required)
+
   protected category = new FormControl<CategoryType>('audiobook', Validators.required)
+  protected folderName = new FormControl<string>('', Validators.required)
+  protected folderImageUrl = new FormControl<string>('')
+  protected sorting = new FormControl<Sorting>(Sorting.AlphabeticalAscending, Validators.required)
+
+  protected title = new FormControl<string>('', Validators.required)
+  protected coverImageUrl = new FormControl<string>('')
+
+  protected shuffle = new FormControl<boolean>(false)
+  protected interval = new FormControl<boolean>(false)
 
   // If the source is Spotify (not Spotify query), we allow the folder name and cover image
   // to be automatically deduced. In that case, we want to simplify the UI by hiding the (optional)
@@ -81,10 +91,18 @@ export class AddEditPage {
   protected automaticFolderNameAndImage = model(true)
 
   protected sourceType = toSignal(this.sourceTypeControl.valueChanges)
+  protected intervalSignal = toSignal(this.interval.valueChanges)
 
   protected formGroup = new FormGroup({
     sourceType: this.sourceTypeControl,
     sourceUrl: this.sourceUrl,
     category: this.category,
+    sorting: this.sorting,
+    folderName: this.folderName,
+    folderImageUrl: this.folderImageUrl,
+    title: this.title,
+    coverImageUrl: this.coverImageUrl,
+    shuffle: this.shuffle,
+    interval: this.interval,
   })
 }
